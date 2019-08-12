be4c8fa9b5715d3b73e74317cc6d4cfd00eac4e2
Jesper van den Ende <jespertheend@gmail.com>
2019-08-12 11:52:11 +0200

    initial game object children and parent management

--- src/core/GameObject.js
+++ src/core/GameObject.js
@@ -1,7 +1,15 @@
 import Component from "./components/Component.js";
 
 export default class GameObject{
-	constructor(){
+	constructor(opts){
+		opts = {...{
+			name: "",
+			parent: null,
+		}, ...opts}
+		this.name = opts.name;
+		this._parent = null;
+		this.parent = opts.parent;
+		this._children = [];
 		this.components = [];
 	}
 
@@ -12,5 +20,44 @@ export default class GameObject{
 
 		this.components.push(component);
 		component.attachedToObject(this);
+	}
+
+	get parent(){
+		return this._parent;
+	}
+
+	set parent(newParent){
+		this.setParent(newParent);
+	}
+
+	setParent(newParent, keepPosition = true){
+		if(newParent){
+			newParent.add(this, keepPosition);
+		}else{
+			this._parent = null;
+		}
+	}
+
+	add(child, keepPosition = true){
+		if(child._parent){
+			child._parent.remove(child);
+		}
+		child._parent = this;
+		this._children.push(child);
+	}
+
+	remove(child){
+		this._children = this._children.filter(c => c != child);
+		child._parent = null;
+	}
+
+	*children(){
+		for(const child of this._children){
+			yield child;
+		}
+	}
+
+	getChildren(){
+		return Array.from(this.children());
 	}
 }
