f01dcc989ad1999e7493291fc2e185c07c854af6
Jesper van den Ende <jespertheend@gmail.com>
2020-02-08 05:29:09 +0100

    Revert "initial setup for properties GUI"
    This reverts commit adebecd916ffc8f1d92d53f39f3969f8266e627d.

--- editor/src/PropertiesWindowContent/PropertiesWindowGameObjectContent.js
+++ editor/src/PropertiesWindowContent/PropertiesWindowGameObjectContent.js
@@ -1,17 +1,9 @@
 import PropertiesWindowContent from "./PropertiesWindowContent.js";
 import {GameObject, Vector3} from "../../../../src/index.js";
-import PropertiesGUI from "../UI/PropertiesGUI/PropertiesGUI.js";
 
 export default class PropertiesWindowGameObjectContent extends PropertiesWindowContent{
 	constructor(){
 		super();
-
-		this.propertiesGUI = new PropertiesGUI();
-		this.el.appendChild(this.propertiesGUI.el);
-		this.propertiesGUI.addProperty({label: "Position"});
-		this.propertiesGUI.addProperty({label: "Rotation"});
-		this.propertiesGUI.addProperty({label: "Scale"});
-		this.propertiesGUI.addCollapsable({displayName:"Transform"});
 	}
 
 	static get useForTypes(){
--- editor/src/UI/PropertiesGUI/PropertiesGUIItem.js
+++
@@ -1,6 +0,0 @@
-export default class PropertiesGUIItem{
-	constructor(){
-		this.el = document.createElement("div");
-		this.el.classList.add("propertiesGUIItem");
-	}
-}
