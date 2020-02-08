baf448acad54939605c4b7ac4629b2c6e51207e2
Jesper van den Ende <jespertheend@gmail.com>
2020-02-08 05:44:10 +0100

    add temporary cube to scene on page load for development purposes

--- editor/src/editorInstance.js
+++ editor/src/editorInstance.js
@@ -5,3 +5,7 @@ window["editor"] = editor;
 export default editor;
 
 editor.init();
+
+//temporary: create a cube
+//remove this later when project management is working correctly
+editor.windowManager.rootWindow.windowB.windowA.windowA.tabs[0].newEmptyEditingObject();
