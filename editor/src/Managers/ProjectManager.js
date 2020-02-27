2712d19fa7601523c254f77349318bcf17dcecca
Jesper van den Ende <jespertheend@gmail.com>
2020-02-27 20:34:54 +0100

    initial asset manager

--- editor/src/Managers/ProjectManager.js
+++ editor/src/Managers/ProjectManager.js
@@ -1,15 +1,25 @@
 import editor from "../editorInstance.js";
 import EditorFileSystemNative from "../Util/FileSystems/EditorFileSystemNative.js";
 import EditorFileSystemIndexedDB from "../Util/FileSystems/EditorFileSystemIndexedDB.js";
+import AssetManager from "./AssetManager.js";
 
 export default class ProjectManager{
 	constructor(){
 		this.currentProjectFileSystem = null;
+		this.assetManager = null;
+		this.reloadAssetManager();
 	}
 
 	openProject(fileSystem){
 		this.currentProjectFileSystem = fileSystem;
 		editor.windowManager.reloadCurrentWorkspace();
+	}
+
+	reloadAssetManager(){
+		if(this.assetManager){
+			this.assetManager.destructor();
+		}
+		this.assetManager = new AssetManager();
 	}
 
 	async openProjectFromLocalDirectory(){
