3b9bd01cf0122eec6b34fa09eed1f9ff014366a9
Jesper van den Ende <jespertheend@gmail.com>
2020-09-10 22:58:47 +0200

    initial asset bundle settings gui

--- editor/src/PropertiesAssetContent/PropertiesAssetContentAssetBundle.js
+++ editor/src/PropertiesAssetContent/PropertiesAssetContentAssetBundle.js
@@ -1,8 +1,21 @@
 import PropertiesAssetContent from "./PropertiesAssetContent.js";
+import {Mesh} from "../../../src/index.js";
 
 export default class PropertiesAssetContentAssetBundle extends PropertiesAssetContent{
 	constructor(){
 		super();
-		const materialSettingsTree = this.treeView.addCollapsable("asset bundle settings");
+		const bundleSettingsTree = this.treeView.addCollapsable("asset bundle settings");
+		this.assetsArrayGui = bundleSettingsTree.addItem({
+			type: Array,
+			guiItemOpts:{
+				arrayTypeOpts:{
+					type: Mesh,
+				}
+			}
+		});
+	}
+
+	updateAll(selectedBundles){
+		console.log(selectedBundles);
 	}
 }
