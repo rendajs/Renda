507e98fe461ef1d0823899ff5a7df745473eedc3
Jesper van den Ende <jespertheend@gmail.com>
2020-09-12 01:17:24 +0200

    add onvaluechange callback support to TextGui

--- editor/src/UI/TextGui.js
+++ editor/src/UI/TextGui.js
@@ -2,10 +2,15 @@ export default class TextGui{
 	constructor(){
 		this.el = document.createElement("input");
 		this.el.classList.add("textGui", "buttonLike", "resetInput", "textInput");
+
+		this.onValueChangeCbs = new Set();
+		this.boundFireOnChangeCbs = this.fireOnChangeCbs.bind(this);
+		this.el.addEventListener("change", this.boundFireOnChangeCbs);
 	}
 
 	destructor(){
-
+		this.el.removeEventListener("change", this.boundFireOnChangeCbs);
+		this.boundFireOnChangeCbs = null;
 	}
 
 	setValue(value){
@@ -14,5 +19,15 @@ export default class TextGui{
 
 	get value(){
 		return this.el.value;
+	}
+
+	onValueChange(cb){
+		this.onValueChangeCbs.add(cb);
+	}
+
+	fireOnChangeCbs(){
+		for(const cb of this.onValueChangeCbs){
+			cb(this.value);
+		}
 	}
 }
