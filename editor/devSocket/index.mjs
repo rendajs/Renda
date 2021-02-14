d20cc2361be5e42f3eece5d5d55b64b77a31d04e
Jesper van den Ende <jespertheend@gmail.com>
2021-02-14 16:49:35 +0100

    move devSocket to dedicated folder

--- editor/devSocket/index.mjs
+++ editor/devSocket/index.mjs
@@ -32,7 +32,7 @@ function sendAllConnections(data){
 }
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const builtInAssetsPath = path.resolve(__dirname, "builtInAssets/");
+const builtInAssetsPath = path.resolve(__dirname, "../builtInAssets/");
 console.log("watching for file changes in " + builtInAssetsPath);
 fs.watch(builtInAssetsPath, {recursive:true}, (eventType, filename) => {
 	sendAllConnections({
