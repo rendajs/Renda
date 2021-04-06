f791189588ef23e07e25617db73826d00d0ceb10
Jesper van den Ende <jespertheend@gmail.com>
2021-04-06 21:05:34 +0200

    make cameragizmo projection matrix work

--- src/Gizmos/Gizmos/CameraGizmo.js
+++ src/Gizmos/Gizmos/CameraGizmo.js
@@ -2,6 +2,7 @@ import Gizmo from "./Gizmo.js";
 import Mesh from "../../Core/Mesh.js";
 import DefaultComponentTypes from "../../Components/DefaultComponentTypes.js";
 import Vec3 from "../../Math/Vec3.js";
+import Vec4 from "../../Math/Vec4.js";
 import Mat4 from "../../Math/Mat4.js";
 
 export default class CameraGizmo extends Gizmo{
@@ -12,16 +13,6 @@ export default class CameraGizmo extends Gizmo{
 		this.mesh.setVertexState(this.gizmoManager.meshVertexState);
 
 		const indices = [0,1,1,3,3,2,2,0,  0,4, 1,5, 2,6, 3,7,  4,5,5,7,7,6,6,4];
-		const positions = [
-			new Vec3(-1,-1,-1),
-			new Vec3(-1,-1, 1),
-			new Vec3(-1, 1,-1),
-			new Vec3(-1, 1, 1),
-			new Vec3( 1,-1,-1),
-			new Vec3( 1,-1, 1),
-			new Vec3( 1, 1,-1),
-			new Vec3( 1, 1, 1),
-		];
 		const colors = [
 			new Vec3(1,1,1),
 			new Vec3(1,1,1),
@@ -38,25 +29,29 @@ export default class CameraGizmo extends Gizmo{
 			materials: [this.gizmoManager.meshMaterial],
 		});
 
-		this.mesh.setVertexCount(positions.length);
+		this.mesh.setVertexCount(8);
 		this.mesh.setIndexData(indices);
 		this.mesh.setVertexData(Mesh.AttributeType.COLOR, colors);
 	}
 
 	setProjectionMatrix(projection){
-		const positions = [
-			new Vec3(-1,-1,-1),
+		const positionsCube = [
+			new Vec3(-1,-1, 0),
 			new Vec3(-1,-1, 1),
-			new Vec3(-1, 1,-1),
+			new Vec3(-1, 1, 0),
 			new Vec3(-1, 1, 1),
-			new Vec3( 1,-1,-1),
+			new Vec3( 1,-1, 0),
 			new Vec3( 1,-1, 1),
-			new Vec3( 1, 1,-1),
+			new Vec3( 1, 1, 0),
 			new Vec3( 1, 1, 1),
 		];
-		for(const pos of positions){
-			pos.multiplyMatrix(projection);
-		}
-		this.mesh.setVertexData(Mesh.AttributeType.POSITION, positions);
+		const inverse = projection.inverse();
+		const positionsFrustum = positionsCube.map(pos => {
+			const pos4 = new Vec4(pos);
+			pos4.multiplyMatrix(inverse);
+			pos4.divide(pos4.w);
+			return new Vec3(pos4);
+		});
+		this.mesh.setVertexData(Mesh.AttributeType.POSITION, positionsFrustum);
 	}
 }
