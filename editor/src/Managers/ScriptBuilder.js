b4ab06dbe203a9e90bc3b67ca5a59fbe776b3e74
Jesper van den Ende <jespertheend@gmail.com>
2021-05-04 22:13:39 +0200

    add some more webgpu externs to scriptbuilder

--- editor/src/Managers/ScriptBuilder.js
+++ editor/src/Managers/ScriptBuilder.js
@@ -23,17 +23,36 @@ export default class ScriptBuilder{
 			const {exitCode, stdErr, stdOut} = await editor.devSocket.sendRoundTripMessage("runClosureCompiler", {
 				js: code,
 				externs: `
+					navigator.gpu = {}
+					navigator.gpu.requestAdapter = function(){}
+
 					const GPUMapMode = {};
-					GPUMapMode.READ = {};
+					GPUMapMode.READ = 0;
+					GPUMapMode.WRITE = 0;
 
 					const GPUTextureUsage = {};
-					GPUTextureUsage.RENDER_ATTACHMENT = {};
+					GPUTextureUsage.COPY_SRC = 0;
+					GPUTextureUsage.COPY_DST = 0;
+					GPUTextureUsage.SAMPLED = 0;
+					GPUTextureUsage.STORAGE = 0;
+					GPUTextureUsage.RENDER_ATTACHMENT = 0;
 
 					const GPUBufferUsage = {};
-					GPUBufferUsage.UNIFORM = {};
+					GPUBufferUsage.MAP_READ = 0;
+					GPUBufferUsage.MAP_WRITE = 0;
+					GPUBufferUsage.COPY_SRC = 0;
+					GPUBufferUsage.COPY_DST = 0;
+					GPUBufferUsage.INDEX = 0;
+					GPUBufferUsage.VERTEX = 0;
+					GPUBufferUsage.UNIFORM = 0;
+					GPUBufferUsage.STORAGE = 0;
+					GPUBufferUsage.INDIRECT = 0;
+					GPUBufferUsage.QUERY_RESOLVE = 0;
 
 					const GPUShaderStage = {};
-					GPUShaderStage.VERTEX = {};
+					GPUShaderStage.VERTEX = 0;
+					GPUShaderStage.FRAGMENT = 0;
+					GPUShaderStage.COMPUTE = 0;
 				`,
 				args: {
 					compilation_level: "ADVANCED",
