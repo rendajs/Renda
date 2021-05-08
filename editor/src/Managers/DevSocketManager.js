061c3c54d00bf17a54387460ac6dcd8c8f230a10
Jesper van den Ende <jespertheend@gmail.com>
2021-05-08 22:12:05 +0200

    attempt reconnecting to devsocket after connection close

--- editor/src/Managers/DevSocketManager.js
+++ editor/src/Managers/DevSocketManager.js
@@ -3,10 +3,57 @@ export default class DevSocketManager{
 		this.listeners = new Map();
 		this.roundTripCbs = new Set();
 
-		this.ws = new WebSocket("ws://localhost:5071");
-		this.ws.addEventListener("message", e => this.handleMessage(e));
+		this.ws = null;
+		this.connectedOnce = false;
+		this.isTryingConnectMultiple = false;
 
 		this.lastRoundtripId = 0;
+
+		this.tryConnectionOnce();
+	}
+
+	get connected(){
+		if(!this.ws) return false;
+		return this.ws.readyState == WebSocket.OPEN;
+	}
+
+	async tryConnectionOnce(){
+		const ws = this.ws = new WebSocket("ws://localhost:5071");
+		this.ws.addEventListener("message", e => {
+			if(this.ws != ws) return;
+			this.handleMessage(e);
+		});
+		return await new Promise(resolve => {
+			this.ws.addEventListener("open", e => {
+				if(this.ws != ws) return;
+				this.connectedOnce = true;
+				resolve(true);
+			});
+			this.ws.addEventListener("close", e => {
+				if(this.ws != ws) return;
+				this.ws = null;
+				resolve(false);
+				this.handleClose();
+			});
+		});
+	}
+
+	async tryConnectionMultiple(){
+		if(this.isTryingConnectMultiple) return;
+		this.isTryingConnectMultiple = true;
+		let attempts = 0;
+		while(true){
+			attempts++;
+			if(attempts > 3) return false;
+
+			await new Promise(r => setTimeout(r, attempts * 1000));
+
+			const success = await this.tryConnectionOnce();
+			if(success){
+				this.isTryingConnectMultiple = false;
+				return true;
+			}
+		}
 	}
 
 	handleMessage(e){
@@ -24,6 +71,12 @@ export default class DevSocketManager{
 			for(const cb of cbs){
 				cb(data.data);
 			}
+		}
+	}
+
+	handleClose(){
+		if(this.connectedOnce){
+			this.tryConnectionMultiple();
 		}
 	}
 
