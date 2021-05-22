215346b09be4fa5013e23291580e52743b615a49
Jesper van den Ende <jespertheend@gmail.com>
2021-05-23 01:31:15 +0200

    Add engine asset handlers define

--- src/Assets/EngineAssetsManager.js
+++ src/Assets/EngineAssetsManager.js
@@ -1,19 +1,24 @@
 import defaultAssetLoader from "./defaultAssetLoader.js";
+import {ENABLE_ENGINE_ASSETS_HANDLERS} from "../defines.js";
 
 export default class EngineAssetsManager{
 	constructor(){
+		if(!ENABLE_ENGINE_ASSETS_HANDLERS) return;
 		this.getAssetHandlers = new Set();
 	}
 
 	async getAsset(...args){
-		for(const handler of this.getAssetHandlers){
-			const result = await handler(...args);
-			if(result) return result;
+		if(ENABLE_ENGINE_ASSETS_HANDLERS){
+			for(const handler of this.getAssetHandlers){
+				const result = await handler(...args);
+				if(result) return result;
+			}
 		}
 		return await defaultAssetLoader.getAsset(...args);
 	}
 
 	addGetAssetHandler(handlerFunction){
+		if(!ENABLE_ENGINE_ASSETS_HANDLERS) return;
 		this.getAssetHandlers.add(handlerFunction);
 	}
 }
--- src/defines.js
+++ src/defines.js
@@ -2,4 +2,17 @@
 //Changing these defines will cause certain parts of code
 //to get stripped, making the build size smaller
 
+
+
+/* ==== Generic Defines ==== */
+
 export const ENABLE_WEBGPU_CLUSTERED_LIGHTS = true;
+
+
+
+/* ======== Editor Defines ======== */
+//These are defines that are generally only needed for the
+//editor. Most builds can set all of these to false.
+
+//enables support for adding handlers to EngineAssetsManager
+export const ENABLE_ENGINE_ASSETS_HANDLERS = true;
