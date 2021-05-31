cb7bf773573450adadbb7631c0a8638684f08e3d
Jesper van den Ende <jespertheend@gmail.com>
2021-05-31 23:06:21 +0200

    Add cluster config property to cam component

--- src/Components/BuiltIn/CameraComponent.js
+++ src/Components/BuiltIn/CameraComponent.js
@@ -1,6 +1,7 @@
 import Mat4 from "../../Math/Mat4.js";
 import BinaryComposer from "../../Util/BinaryComposer.js";
 import RenderOutputConfig from "../../Rendering/RenderOutputConfig.js";
+import ClusteredLightsConfig from "../../Rendering/ClusteredLightsConfig.js";
 
 export default {
 	uuid: "1a78b3f2-7688-4776-b512-ed1ee2326d8a",
@@ -39,6 +40,9 @@ export default {
 		},
 		renderOutputConfig: {
 			type: RenderOutputConfig,
+		},
+		clusteredLightsConfig: {
+			type: ClusteredLightsConfig,
 		},
 		// autoManageRootRenderEntities: {
 		// 	type: "bool",
--- src/Rendering/ClusteredLightsConfig.js
+++ src/Rendering/ClusteredLightsConfig.js
@@ -1,5 +1,11 @@
+import {Vec3} from "../index.js";
+
 export default class ClusteredLightsConfig{
-	constructor(){
-	
+	constructor({
+		clusterCount = [16,9,24],
+		maxLightsPerClusterPass = 10,
+	}){
+		this.clusterCount = new Vec3(clusterCount);
+		this.maxLightsPerClusterPass = maxLightsPerClusterPass;
 	}
 }
