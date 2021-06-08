efe958ed64564bdc2469feff2334315d64604c18
Jesper van den Ende <jespertheend@gmail.com>
2021-06-08 19:44:29 +0200

    Add default vertex state to built in assets

--- editor/src/Assets/AutoRegisterBuiltInAssetLinks.js
+++ editor/src/Assets/AutoRegisterBuiltInAssetLinks.js
@@ -4,4 +4,9 @@ export default [
 		defaultAssetUuid: "f1e469e3-b463-4542-952a-091487bf5b4a",
 		originalAssetUuid: "542fb96a-d3f8-4150-9963-9f1bf803da67",
 	},
+	{
+		name: "Default Vertex State",
+		defaultAssetUuid: "ad4146d6-f709-422e-b93e-5beb51e38fe4",
+		originalAssetUuid: "36996838-4f7e-4be3-9c18-49c4a9db00be",
+	}
 ];
