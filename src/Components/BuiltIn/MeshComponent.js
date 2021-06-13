659031c10ddad79302b873663e25df12d78a9ab7
Jesper van den Ende <jespertheend@gmail.com>
2021-06-13 13:10:00 +0200

    Add engine define for default asset uuids in components

--- src/Components/BuiltIn/MeshComponent.js
+++ src/Components/BuiltIn/MeshComponent.js
@@ -1,8 +1,9 @@
 import Mesh from "../../Core/Mesh.js";
+import { EDITOR_DEFAULTS_IN_COMPONENTS } from "../../engineDefines.js";
 import Material from "../../Rendering/Material.js";
 import BinaryComposer from "../../Util/BinaryComposer.js";
 
-export default {
+const MeshComponent = {
 	uuid: "c7fc3a04-fa51-49aa-8f04-864c0cebf49c",
 	name: "Mesh",
 	properties: {
@@ -27,3 +28,10 @@ export default {
 		},
 	},
 };
+
+if(EDITOR_DEFAULTS_IN_COMPONENTS){
+	const defaultMaterialAssetLinkUuid = "f1e469e3-b463-4542-952a-091487bf5b4a";
+	MeshComponent.properties.materials.defaultValue = [defaultMaterialAssetLinkUuid];
+}
+
+export default MeshComponent;
--- src/engineDefines.js
+++ src/engineDefines.js
@@ -16,4 +16,11 @@ export const ENABLE_WEBGPU_CLUSTERED_LIGHTS = true;
 
 //enables listening for asset changes for assets that are used by the engine
 export const ENGINE_ASSETS_LIVE_UPDATES_SUPPORT = true;
+
+//Support for storing default asset link uuids as metadata in entities.
+//This is only needed in the editor since only the editor can handle default asset links.
 export const DEFAULT_ASSET_LINKS_IN_ENTITY_JSON_EXPORT = true;
+
+//Apply built-in default asset link uuids to the default values for components.
+//These uuids are only available in the editor, asset bundles remove default asset links when bundling.
+export const EDITOR_DEFAULTS_IN_COMPONENTS = true;
