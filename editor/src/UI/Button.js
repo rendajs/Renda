145b350d5b79e8e33027fe29b3769ae77eaa5694
Jesper van den Ende <jespertheend@gmail.com>
2021-09-06 21:28:17 +0200

    Add support for draggable buttons

--- editor/src/UI/Button.js
+++ editor/src/UI/Button.js
@@ -17,24 +17,29 @@ export default class Button {
 		hasDownArrow = false,
 		onClick = null,
 		disabled = false,
-	} = {}){
-		this.el = document.createElement("div");
-		this.el.classList.add("button", "buttonLike");
+		draggable = false,
+	} = {}) {
+		this.iconUrl = icon;
+		this.currentText = text;
+		const {el, iconEl, textEl} = this.createButtonEl();
+		this.el = el;
+		this.iconEl = iconEl;
+		this.textEl = textEl;
+
 		this.onClick = onClick;
 		this.boundClick = this.click.bind(this);
 		this.el.addEventListener("click", this.boundClick);
 		this.disabled = disabled;
 
-		this.iconEl = document.createElement("div");
-		this.iconEl.classList.add("buttonIcon");
-		this.el.appendChild(this.iconEl);
+		if (draggable) {
+			this.el.draggable = true;
+			this.boundDragStart = this.dragStart.bind(this);
+			this.el.addEventListener("dragstart", this.boundDragStart);
+			this.boundDragEnd = this.dragEnd.bind(this);
+			this.el.addEventListener("dragend", this.boundDragEnd);
+		}
+		this.dragFeedbackEl = null;
 
-		this.textEl = document.createElement("span");
-		this.textEl.classList.add("buttonText");
-		this.el.appendChild(this.textEl);
-
-		this.setText(text);
-		this.setIcon(icon);
 		this.setDisabled(disabled);
 
 		this.onContextMenuCbs = new Set();
@@ -48,6 +53,23 @@ export default class Button {
 		this.el.removeEventListener("contextmenu", this.boundFireContextMenuCbs);
 	}
 
+	createButtonEl() {
+		const el = document.createElement("div");
+		el.classList.add("button", "buttonLike");
+
+		const iconEl = document.createElement("div");
+		iconEl.classList.add("buttonIcon");
+		el.appendChild(iconEl);
+		this.applyIconToEl(iconEl);
+
+		const textEl = document.createElement("span");
+		textEl.classList.add("buttonText");
+		textEl.textContent = this.currentText;
+		el.appendChild(textEl);
+
+		return {el, iconEl, textEl};
+	}
+
 	click(){
 		if(this.disabled) return;
 		if(this.onClick) this.onClick();
@@ -56,7 +78,8 @@ export default class Button {
 	/**
 	 * @param {string} text
 	 */
-	setText(text){
+	setText(text) {
+		this.currentText = text;
 		this.textEl.textContent = text;
 	}
 
@@ -73,10 +96,15 @@ export default class Button {
 	 * @param {string} iconUrl
 	 */
 	setIcon(iconUrl) {
-		this.iconEl.style.backgroundImage = `url(${iconUrl})`;
-		this.iconEl.style.display = iconUrl ? null : "none";
-		if (iconUrl) {
-			editor.colorizerFilterManager.applyFilter(this.iconEl, "var(--default-button-text-color)");
+		this.iconUrl = iconUrl;
+		this.applyIconToEl(this.iconEl);
+	}
+
+	applyIconToEl(el) {
+		el.style.backgroundImage = `url(${this.iconUrl})`;
+		el.style.display = this.iconUrl ? null : "none";
+		if (this.iconUrl) {
+			editor.colorizerFilterManager.applyFilter(el, "var(--default-button-text-color)");
 		}
 	}
 
@@ -99,5 +127,26 @@ export default class Button {
 
 	removeOnContextMenu(cb) {
 		this.onContextMenuCbs.delete(cb);
+	}
+
+	/**
+	 * @param {DragEvent} e
+	 */
+	dragStart(e) {
+		if (!this.dragFeedbackEl) {
+			const {el}	= this.createButtonEl();
+			this.dragFeedbackEl = el;
+			el.style.position = "absolute";
+			el.style.transform = "translateX(-100%)";
+			document.body.appendChild(this.dragFeedbackEl);
+		}
+		e.dataTransfer.setDragImage(this.dragFeedbackEl, this.dragFeedbackEl.offsetWidth / 2, this.dragFeedbackEl.offsetHeight / 2);
+	}
+
+	dragEnd(e) {
+		if (this.dragFeedbackEl) {
+			this.dragFeedbackEl.parentElement.removeChild(this.dragFeedbackEl);
+			this.dragFeedbackEl = null;
+		}
 	}
 }
