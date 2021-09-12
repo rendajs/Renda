83c60736d7196f9d637808727c9c67c698f9c540
Jesper van den Ende <jespertheend@gmail.com>
2021-09-12 18:44:31 +0200

    Apply eslint to ProjectManager

--- editor/src/Managers/ProjectManager.js
+++ editor/src/Managers/ProjectManager.js
@@ -3,22 +3,20 @@ import EditorFileSystemNative from "../Util/FileSystems/EditorFileSystemNative.j
 import EditorFileSystemIndexedDB from "../Util/FileSystems/EditorFileSystemIndexedDB.js";
 import AssetManager from "../Assets/AssetManager.js";
 import IndexedDbUtil from "../Util/IndexedDbUtil.js";
-/** @typedef {import("../Util/FileSystems/EditorFileSystem.js").default} EditorFileSystem */
 
-export default class ProjectManager{
-	constructor(){
-		/**
-		 * @type {?EditorFileSystem}
-		 */
+export default class ProjectManager {
+	constructor() {
+		/** @type {?import("../Util/FileSystems/EditorFileSystem.js").default} */
 		this.currentProjectFileSystem = null;
 		this.assetManager = null;
+		this.editorConnectionServer = null;
 
 		this.tmpNativeHandleDb = new IndexedDbUtil("tmpNFShandles");
 
 		this.onExternalChangeCbs = new Set();
 		window.addEventListener("focus", () => this.suggestCheckExternalChanges());
 		document.addEventListener("visibilitychange", () => {
-			if(document.visibilityState === "visible"){
+			if (document.visibilityState === "visible") {
 				this.suggestCheckExternalChanges();
 			}
 		});
@@ -26,11 +24,14 @@ export default class ProjectManager{
 		this.onAssetManagerLoadCbs = new Set();
 	}
 
-	openProject(fileSystem){
+	/**
+	 * @param {import("../Util/FileSystems/EditorFileSystem.js").default} fileSystem
+	 */
+	openProject(fileSystem) {
 		this.currentProjectFileSystem = fileSystem;
-		//todo remove this event when opening a new fileSystem
+		// todo remove this event when opening a new fileSystem
 		fileSystem.onExternalChange(e => {
-			for(const cb of this.onExternalChangeCbs){
+			for (const cb of this.onExternalChangeCbs) {
 				cb(e);
 			}
 		});
@@ -38,52 +39,52 @@ export default class ProjectManager{
 		this.reloadAssetManager();
 	}
 
-	async reloadAssetManager(){
-		if(this.assetManager){
+	async reloadAssetManager() {
+		if (this.assetManager) {
 			this.assetManager.destructor();
 		}
 		this.assetManager = new AssetManager();
 		await this.assetManager.waitForAssetSettingsLoad();
-		for(const cb of this.onAssetManagerLoadCbs){
+		for (const cb of this.onAssetManagerLoadCbs) {
 			cb();
 		}
 		this.onAssetManagerLoadCbs.clear();
 	}
 
-	async waitForAssetManagerLoad(){
-		if(this.assetManager && this.assetManager.assetSettingsLoaded) return;
+	async waitForAssetManagerLoad() {
+		if (this.assetManager && this.assetManager.assetSettingsLoaded) return;
 		await new Promise(r => this.onAssetManagerLoadCbs.add(r));
 	}
 
-	async openProjectFromLocalDirectory(){
+	async openProjectFromLocalDirectory() {
 		const fileSystem = await EditorFileSystemNative.openUserDir();
 		this.tmpNativeHandleDb.set("lastHandle", fileSystem.handle);
 		this.openProject(fileSystem);
 	}
 
-	async openRecentProjectHandle(){
+	async openRecentProjectHandle() {
 		const handle = await this.tmpNativeHandleDb.get("lastHandle");
-		if(handle){
+		if (handle) {
 			const fileSystem = new EditorFileSystemNative(handle);
 			this.openProject(fileSystem);
 		}
 	}
 
-	async openDb(){
-		let fileSystem = new EditorFileSystemIndexedDB("test project");
+	async openDb() {
+		const fileSystem = new EditorFileSystemIndexedDB("test project");
 		this.openProject(fileSystem);
 	}
 
-	onExternalChange(cb){
+	onExternalChange(cb) {
 		this.onExternalChangeCbs.add(cb);
 	}
 
-	removeOnExternalChange(cb){
+	removeOnExternalChange(cb) {
 		this.onExternalChangeCbs.delete(cb);
 	}
 
-	suggestCheckExternalChanges(){
-		if(this.currentProjectFileSystem){
+	suggestCheckExternalChanges() {
+		if (this.currentProjectFileSystem) {
 			this.currentProjectFileSystem.suggestCheckExternalChanges();
 		}
 	}
