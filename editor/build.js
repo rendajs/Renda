1eafacd22f80617d5d9b037d122ada6fcc224937
Jesper van den Ende <jespertheend@gmail.com>
2021-09-13 19:43:42 +0200

    Fix eslint issues for editor build script

--- editor/build.js
+++ editor/build.js
@@ -2,55 +2,57 @@
 
 import {rollup} from "rollup";
 import {promises as fs} from "fs";
+import process from "process";
 
 const isDevBuild = process.argv.includes("--dev");
 
 const defines = {
-	"EDITOR_ENV": isDevBuild ? "dev" : "production",
-	"IS_DEV_BUILD": isDevBuild,
+	EDITOR_ENV: isDevBuild ? "dev" : "production",
+	IS_DEV_BUILD: isDevBuild,
 };
 
-async function setScriptSrc(src){
+async function setScriptSrc(src) {
 	const filePath = "editor/dist/index.html";
-	let data = await fs.readFile(filePath, {encoding: "utf8"});
+	const data = await fs.readFile(filePath, {encoding: "utf8"});
 	let startPos = data.indexOf("<!--editor script tag-->");
-	let searchStr = "<script type=\"module\" src=\"";
+	const searchStr = "<script type=\"module\" src=\"";
 	startPos = data.indexOf(searchStr, startPos);
 	startPos += searchStr.length;
-	let endPos = data.indexOf("\"></script>", startPos);
-	let newData = data.substring(0, startPos)+src+data.substring(endPos, data.length);
+	const endPos = data.indexOf("\"></script>", startPos);
+	const newData = data.substring(0, startPos) + src + data.substring(endPos, data.length);
 	await fs.writeFile(filePath, newData);
 }
 
-function overrideDefines(definesFilePath, defines){
+function overrideDefines(definesFilePath, defines) {
 	return {
 		name: "editor-replace-defines",
-		transform(code, id){
-			if(id.endsWith(definesFilePath)){
-				for(const [name, value] of Object.entries(defines)){
-					const re = new RegExp(name+"\\s?=.+;?$","gm");
+		transform(code, id) {
+			if (id.endsWith(definesFilePath)) {
+				for (const [name, value] of Object.entries(defines)) {
+					const re = new RegExp(name + "\\s?=.+;?$", "gm");
 					code = code.replace(re, `${name} = ${JSON.stringify(value)};`);
 				}
 				return code;
 			}
+			return null;
 		},
-	}
+	};
 }
 
 (async () => {
-	if(isDevBuild){
-		try{
+	if (isDevBuild) {
+		try {
 			await fs.unlink("editor/dist/index.js");
-		}catch(e){
-			//fail silently
+		} catch (e) {
+			// fail silently
 		}
 		await setScriptSrc("../src/index.js");
-	}else{
+	} else {
 		const bundle = await rollup({
 			input: "editor/src/index.js",
 			plugins: [overrideDefines("editor/src/editorDefines.js", defines)],
 			onwarn: message => {
-				if(message.code == "CIRCULAR_DEPENDENCY") return;
+				if (message.code == "CIRCULAR_DEPENDENCY") return;
 				console.error(message.message);
 			},
 		});
