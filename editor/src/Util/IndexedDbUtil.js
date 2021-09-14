287e4244536fc578694a84c89334a075ec812226
Jesper van den Ende <jespertheend@gmail.com>
2021-09-14 23:29:46 +0200

    Apply eslint to IndexedDbUtil

--- editor/src/Util/IndexedDbUtil.js
+++ editor/src/Util/IndexedDbUtil.js
@@ -1,95 +1,95 @@
-export default class IndexedDbUtil{
-	constructor(dbName = "keyValuesDb", objectStoreNames = ["keyValues"]){
+export default class IndexedDbUtil {
+	constructor(dbName = "keyValuesDb", objectStoreNames = ["keyValues"]) {
 		this.dbName = dbName;
 		this.objectStoreNames = objectStoreNames;
 
 		this.supported = false;
-		try{
-			let dbRequest = indexedDB.open(dbName);
+		try {
+			const dbRequest = indexedDB.open(dbName);
 			dbRequest.onupgradeneeded = () => {
-				for(const name of objectStoreNames){
+				for (const name of objectStoreNames) {
 					dbRequest.result.createObjectStore(name);
 				}
 			};
 			this.supported = true;
-		}catch(e){
-			console.log("error while opening indexedDB: ",e);
+		} catch (e) {
+			console.log("error while opening indexedDB: ", e);
 		}
 	}
 
-	async promisifyRequest(request){
-		if(request.readyState == "done") return request.result;
+	async promisifyRequest(request) {
+		if (request.readyState == "done") return request.result;
 		return await new Promise((resolve, reject) => {
 			request.onsuccess = () => {
-				resolve(request.result)
+				resolve(request.result);
 			};
 			request.onerror = reject;
 		});
 	}
 
-	getLocalStorageName(key, objectStoreName = this.objectStoreNames[0]){
-		return "indexedDBFallback-"+this.dbName+"-"+objectStoreName+"-"+key;
+	getLocalStorageName(key, objectStoreName = this.objectStoreNames[0]) {
+		return "indexedDBFallback-" + this.dbName + "-" + objectStoreName + "-" + key;
 	}
 
-	async get(key, objectStoreName = this.objectStoreNames[0]){
-		if(!this.supported){
+	async get(key, objectStoreName = this.objectStoreNames[0]) {
+		if (!this.supported) {
 			let val = localStorage.getItem(this.getLocalStorageName(key, objectStoreName));
-			try{
+			try {
 				val = JSON.parse(val);
-			}catch(e){
+			} catch (e) {
 				val = null;
 			}
 			return val;
 		}
-		let request = await indexedDB.open(this.dbName);
-		let db = await this.promisifyRequest(request);
-		let transaction = db.transaction(objectStoreName, "readonly");
-		let objectStore = transaction.objectStore(objectStoreName);
-		let getRequest = objectStore.get(key);
+		const request = await indexedDB.open(this.dbName);
+		const db = await this.promisifyRequest(request);
+		const transaction = db.transaction(objectStoreName, "readonly");
+		const objectStore = transaction.objectStore(objectStoreName);
+		const getRequest = objectStore.get(key);
 		return await this.promisifyRequest(getRequest);
 	}
 
-	set(key, value, objectStoreName = this.objectStoreNames[0]){
+	set(key, value, objectStoreName = this.objectStoreNames[0]) {
 		return this.getSet(key, () => {
 			return value;
 		}, objectStoreName);
 	}
 
-	async getSet(key, cb, objectStoreName = this.objectStoreNames[0], deleteCursor = false){
-		if(!this.supported){
-			let newKey = this.getLocalStorageName(key, objectStoreName);
+	async getSet(key, cb, objectStoreName = this.objectStoreNames[0], deleteCursor = false) {
+		if (!this.supported) {
+			const newKey = this.getLocalStorageName(key, objectStoreName);
 			let val = localStorage.getItem(newKey);
-			try{
+			try {
 				val = JSON.parse(val);
-			}catch(e){
+			} catch (e) {
 				val = null;
 			}
-			let newVal = cb(val);
+			const newVal = cb(val);
 			localStorage.setItem(newKey, JSON.stringify(newVal));
 			return;
 		}
-		let request = indexedDB.open(this.dbName);
-		let db = await this.promisifyRequest(request);
-		let transaction = db.transaction(objectStoreName, "readwrite");
-		let objectStore = transaction.objectStore(objectStoreName);
-		let cursorRequest = objectStore.openCursor(key);
-		let cursor = await this.promisifyRequest(cursorRequest);
-		if(cursor){
-			if(deleteCursor){
-				let cursorRequest = cursor.delete();
+		const request = indexedDB.open(this.dbName);
+		const db = await this.promisifyRequest(request);
+		const transaction = db.transaction(objectStoreName, "readwrite");
+		const objectStore = transaction.objectStore(objectStoreName);
+		const cursorRequest = objectStore.openCursor(key);
+		const cursor = await this.promisifyRequest(cursorRequest);
+		if (cursor) {
+			if (deleteCursor) {
+				const cursorRequest = cursor.delete();
 				await this.promisifyRequest(cursorRequest);
-			}else{
-				let newVal = cb(cursor.value);
-				let cursorRequest = cursor.update(newVal);
+			} else {
+				const newVal = cb(cursor.value);
+				const cursorRequest = cursor.update(newVal);
 				await this.promisifyRequest(cursorRequest);
 			}
-		}else{
-			let putRequest = objectStore.put(cb(null), key);
+		} else {
+			const putRequest = objectStore.put(cb(null), key);
 			await this.promisifyRequest(putRequest);
 		}
 	}
 
-	delete(key, objectStoreName = this.objectStoreNames[0]){
+	delete(key, objectStoreName = this.objectStoreNames[0]) {
 		this.getSet(key, () => {}, objectStoreName, true);
 	}
 }
