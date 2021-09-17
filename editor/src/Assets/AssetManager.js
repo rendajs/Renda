979bbc5a75510764fa7744cf01963bd1b5c69e7f
Jesper van den Ende <jespertheend@gmail.com>
2021-09-17 21:19:06 +0200

    Apply eslint to assetmanager

--- editor/src/Assets/AssetManager.js
+++ editor/src/Assets/AssetManager.js
@@ -3,8 +3,8 @@ import {generateUuid, handleDuplicateName} from "../Util/Util.js";
 import DefaultAssetLink from "./DefaultAssetLink.js";
 import ProjectAsset from "./ProjectAsset.js";
 
-export default class AssetManager{
-	constructor(){
+export default class AssetManager {
+	constructor() {
 		/**
 		 * @type {Map<string, ProjectAsset>}
 		 */
@@ -24,50 +24,50 @@ export default class AssetManager{
 		editor.projectManager.onExternalChange(this.boundExternalChange);
 	}
 
-	destructor(){
+	destructor() {
 		editor.projectManager.removeOnExternalChange(this.boundExternalChange);
 		this.boundExternalChange = null;
 	}
 
-	get fileSystem(){
+	get fileSystem() {
 		return editor.projectManager.currentProjectFileSystem;
 	}
 
-	get builtInAssets(){
+	get builtInAssets() {
 		return editor.builtInAssetManager.assets;
 	}
 
-	async loadAssetSettings(fromUserEvent = false){
-		if(this.assetSettingsLoaded) return;
+	async loadAssetSettings(fromUserEvent = false) {
+		if (this.assetSettingsLoaded) return;
 
-		if(!fromUserEvent){
+		if (!fromUserEvent) {
 			const hasPermissions = await this.fileSystem.getPermission(this.assetSettingsPath);
-			if(!hasPermissions) return;
+			if (!hasPermissions) return;
 		}
 
-		for(const builtInAssetLink of editor.builtInDefaultAssetLinksManager.registeredAssetLinks){
+		for (const builtInAssetLink of editor.builtInDefaultAssetLinksManager.registeredAssetLinks) {
 			const defaultAssetLink = new DefaultAssetLink(builtInAssetLink);
 			defaultAssetLink.setBuiltIn(true, builtInAssetLink.originalAssetUuid);
 			this.defaultAssetLinks.set(builtInAssetLink.defaultAssetUuid, defaultAssetLink);
 		}
 
-		if(await this.fileSystem.isFile(this.assetSettingsPath)){
-			let json = await this.fileSystem.readJson(this.assetSettingsPath);
-			if(json){
-				for(const [uuid, assetData] of Object.entries(json.assets)){
+		if (await this.fileSystem.isFile(this.assetSettingsPath)) {
+			const json = await this.fileSystem.readJson(this.assetSettingsPath);
+			if (json) {
+				for (const [uuid, assetData] of Object.entries(json.assets)) {
 					const projectAsset = await ProjectAsset.fromJsonData(uuid, assetData);
-					if(projectAsset){
+					if (projectAsset) {
 						projectAsset.makeUuidConsistent();
 						this.projectAssets.set(uuid, projectAsset);
 					}
 				}
 
-				if(json.defaultAssetLinks){
-					for(const [defaultAssetUuid, defaultAssetData] of Object.entries(json.defaultAssetLinks)){
+				if (json.defaultAssetLinks) {
+					for (const [defaultAssetUuid, defaultAssetData] of Object.entries(json.defaultAssetLinks)) {
 						const existingDefaultAssetLink = this.getDefaultAssetLink(defaultAssetUuid);
-						if(existingDefaultAssetLink){
+						if (existingDefaultAssetLink) {
 							existingDefaultAssetLink.setUserData(defaultAssetData);
-						}else{
+						} else {
 							const defaultAssetLink = new DefaultAssetLink({defaultAssetUuid, ...defaultAssetData});
 							this.defaultAssetLinks.set(defaultAssetUuid, defaultAssetLink);
 						}
@@ -75,47 +75,48 @@ export default class AssetManager{
 				}
 			}
 		}
-		for(const cb of this.waitForAssetSettingsLoadCbs){
+		for (const cb of this.waitForAssetSettingsLoadCbs) {
 			cb();
 		}
 		this.assetSettingsLoaded = true;
 	}
 
-	async waitForAssetSettingsLoad(){
-		if(this.assetSettingsLoaded) return;
+	async waitForAssetSettingsLoad() {
+		if (this.assetSettingsLoaded) return;
 		await new Promise(r => this.waitForAssetSettingsLoadCbs.add(r));
 	}
 
-	async saveAssetSettings(){
+	async saveAssetSettings() {
 		const assetSettings = {};
-		let assets = assetSettings.assets = {};
-		for(const [uuid, projectAsset] of this.projectAssets){
-			if(projectAsset.needsAssetSettingsSave){
+		const assets = {};
+		assetSettings.assets = assets;
+		for (const [uuid, projectAsset] of this.projectAssets) {
+			if (projectAsset.needsAssetSettingsSave) {
 				assets[uuid] = projectAsset.toJson();
 			}
 		}
 		assetSettings.defaultAssetLinks = {};
 		let savedDefaultAssetLinksCount = 0;
-		for(const [defaultAssetUuid, assetLink] of this.defaultAssetLinks){
+		for (const [defaultAssetUuid, assetLink] of this.defaultAssetLinks) {
 			const jsonData = assetLink.toJson();
-			if(jsonData){
+			if (jsonData) {
 				assetSettings.defaultAssetLinks[defaultAssetUuid] = jsonData;
 				savedDefaultAssetLinksCount++;
 			}
 		}
-		if(savedDefaultAssetLinksCount == 0){
+		if (savedDefaultAssetLinksCount == 0) {
 			delete assetSettings.defaultAssetLinks;
 		}
 		await this.fileSystem.writeJson(this.assetSettingsPath, assetSettings);
 	}
 
-	async createNewAsset(parentPath, assetType){
+	async createNewAsset(parentPath, assetType) {
 		const type = editor.projectAssetTypeManager.getAssetType(assetType);
-		let fileName = type.newFileName+"."+type.newFileExtension;
+		let fileName = type.newFileName + "." + type.newFileExtension;
 
-		if(this.fileSystem.exists([...parentPath, fileName])){
+		if (this.fileSystem.exists([...parentPath, fileName])) {
 			const existingFiles = await this.fileSystem.readDir(parentPath);
-			fileName = handleDuplicateName(existingFiles, type.newFileName, "."+type.newFileExtension);
+			fileName = handleDuplicateName(existingFiles, type.newFileName, "." + type.newFileExtension);
 		}
 		const newPath = [...parentPath, fileName];
 
@@ -123,67 +124,67 @@ export default class AssetManager{
 		await projectAsset.createNewLiveAssetData();
 	}
 
-	async deleteAsset(path){
+	async deleteAsset(path) {
 		await this.fileSystem.delete(path, true);
 	}
 
-	async registerAsset(path, assetType = null, forceAssetType = false){
+	async registerAsset(path, assetType = null, forceAssetType = false) {
 		await this.loadAssetSettings(true);
 		const uuid = generateUuid();
 		const projectAsset = new ProjectAsset({uuid, path, assetType, forceAssetType});
 		this.projectAssets.set(uuid, projectAsset);
-		if(projectAsset.needsAssetSettingsSave){
+		if (projectAsset.needsAssetSettingsSave) {
 			this.saveAssetSettings();
 		}
 		return projectAsset;
 	}
 
-	async makeAssetUuidConsistent(asset){
-		if(!asset || asset.needsConsistentUuid || asset.isBuiltIn) return;
+	async makeAssetUuidConsistent(asset) {
+		if (!asset || asset.needsConsistentUuid || asset.isBuiltIn) return;
 		asset.makeUuidConsistent();
 		await this.saveAssetSettings();
 	}
 
-	async externalChange(e){
+	async externalChange(e) {
 		const projectAsset = await this.getProjectAssetFromPath(e.path, this.assetSettingsLoaded);
-		if(projectAsset){
+		if (projectAsset) {
 			const guessedType = await ProjectAsset.guessAssetTypeFromFile(e.path);
-			if(guessedType != projectAsset.assetType){
-				//todo
+			if (guessedType != projectAsset.assetType) {
+				// todo
 				console.warn("not yet implemented: changing assetType");
-			}else{
+			} else {
 				await projectAsset.fileChangedExternally();
 			}
 		}
 	}
 
-	setDefaultAssetLinks(builtInDefaultAssetLinks, defaultAssetLinks){
+	setDefaultAssetLinks(builtInDefaultAssetLinks, defaultAssetLinks) {
 		const unsetAssetLinkUuids = new Set(this.defaultAssetLinks.keys());
-		for(const {defaultAssetUuid, originalAssetUuid} of builtInDefaultAssetLinks){
+		for (const {defaultAssetUuid, originalAssetUuid} of builtInDefaultAssetLinks) {
 			const existingDefaultAssetLink = this.getDefaultAssetLink(defaultAssetUuid);
-			if(existingDefaultAssetLink){
+			if (existingDefaultAssetLink) {
 				existingDefaultAssetLink.setUserData({name: "", originalAssetUuid});
-			}else{
+			} else {
 				this.defaultAssetLinks.set(defaultAssetUuid, new DefaultAssetLink({defaultAssetUuid, originalAssetUuid}));
 			}
 		}
-		let userDefaultAssetLinkUuids = [];
-		for(const {defaultAssetUuid: uuid, name, originalAssetUuid} of defaultAssetLinks){
+		const userDefaultAssetLinkUuids = [];
+		for (const {defaultAssetUuid: uuid, name, originalAssetUuid} of defaultAssetLinks) {
 			let defaultAssetUuid = uuid;
-			if(!defaultAssetUuid) defaultAssetUuid = generateUuid();
+			if (!defaultAssetUuid) defaultAssetUuid = generateUuid();
 			userDefaultAssetLinkUuids.push(defaultAssetUuid);
 			unsetAssetLinkUuids.delete(defaultAssetUuid);
 			const existingDefaultAssetLink = this.getDefaultAssetLink(defaultAssetUuid);
 			const userData = {name, defaultAssetUuid, originalAssetUuid};
-			if(existingDefaultAssetLink){
+			if (existingDefaultAssetLink) {
 				existingDefaultAssetLink.setUserData(userData);
-			}else{
+			} else {
 				this.defaultAssetLinks.set(defaultAssetUuid, new DefaultAssetLink(userData));
 			}
 		}
-		for(const unsetUuid of unsetAssetLinkUuids){
+		for (const unsetUuid of unsetAssetLinkUuids) {
 			const defaultAssetLink = this.getDefaultAssetLink(unsetUuid);
-			if(defaultAssetLink && !defaultAssetLink.isBuiltIn){
+			if (defaultAssetLink && !defaultAssetLink.isBuiltIn) {
 				this.defaultAssetLinks.delete(unsetUuid);
 			}
 		}
@@ -195,21 +196,21 @@ export default class AssetManager{
 	 * @param {string} defaultAssetUuid
 	 * @returns {DefaultAssetLink}
 	 */
-	getDefaultAssetLink(defaultAssetUuid){
+	getDefaultAssetLink(defaultAssetUuid) {
 		return this.defaultAssetLinks.get(defaultAssetUuid);
 	}
 
-	resolveDefaultAssetLinkUuid(uuid){
+	resolveDefaultAssetLinkUuid(uuid) {
 		const defaultAssetLink = this.getDefaultAssetLink(uuid);
-		if(defaultAssetLink){
+		if (defaultAssetLink) {
 			return this.resolveDefaultAssetLinkUuid(defaultAssetLink.originalAssetUuid);
 		}
 		return uuid;
 	}
 
-	async getAssetUuidFromPath(path = []){
+	async getAssetUuidFromPath(path = []) {
 		const projectAsset = await this.getProjectAssetFromPath(path);
-		if(!projectAsset) return null;
+		if (!projectAsset) return null;
 		return projectAsset.uuid;
 	}
 
@@ -217,23 +218,23 @@ export default class AssetManager{
 	 * @param {string} uuid
 	 * @returns {Promise<ProjectAsset>}
 	 */
-	async getProjectAsset(uuid){
+	async getProjectAsset(uuid) {
 		await this.loadAssetSettings(true);
 		return this.getProjectAssetImmediate(uuid);
 	}
 
-	getProjectAssetImmediate(uuid){
-		if(!this.assetSettingsLoaded) return null;
+	getProjectAssetImmediate(uuid) {
+		if (!this.assetSettingsLoaded) return null;
 
 		uuid = this.resolveDefaultAssetLinkUuid(uuid);
 		return this.projectAssets.get(uuid) || this.builtInAssets.get(uuid);
 	}
 
-	async getAssetPathFromUuid(uuid){
+	async getAssetPathFromUuid(uuid) {
 		await this.loadAssetSettings(true);
 		const asset = this.projectAssets.get(uuid);
-		if(!asset){
-			if(this.builtInAssets.has(uuid)){
+		if (!asset) {
+			if (this.builtInAssets.has(uuid)) {
 				throw new Error("Getting asset path from built-in assets is not supported.");
 			}
 			return null;
@@ -241,66 +242,66 @@ export default class AssetManager{
 		return asset.path.slice();
 	}
 
-	static testPathMatch(path1 = [], path2 = []){
-		if(path1.length != path2.length) return false;
-		for(let i=0; i<path1.length; i++){
-			if(path1[i] != path2[i]) return false;
+	static testPathMatch(path1 = [], path2 = []) {
+		if (path1.length != path2.length) return false;
+		for (let i = 0; i < path1.length; i++) {
+			if (path1[i] != path2[i]) return false;
 		}
 		return true;
 	}
 
-	async getProjectAssetFromPath(path = [], registerIfNecessary = true){
+	async getProjectAssetFromPath(path = [], registerIfNecessary = true) {
 		await this.loadAssetSettings(true);
-		for(const [uuid, asset] of this.projectAssets){
-			if(AssetManager.testPathMatch(path, asset.path)){
+		for (const asset of this.projectAssets.values()) {
+			if (AssetManager.testPathMatch(path, asset.path)) {
 				return asset;
 			}
 		}
 
-		if(registerIfNecessary && await this.fileSystem.isFile(path)){
+		if (registerIfNecessary && await this.fileSystem.isFile(path)) {
 			return await this.registerAsset(path);
 		}
 		return null;
 	}
 
-	async assetMoved(fromPath = [], toPath = []){
+	async assetMoved(fromPath = [], toPath = []) {
 		const asset = await this.getProjectAssetFromPath(fromPath);
-		if(!asset) return;
+		if (!asset) return;
 		asset.assetMoved(toPath);
 		await this.saveAssetSettings();
 	}
 
-	async getAssetSettings(path = []){
+	async getAssetSettings(path = []) {
 		await this.loadAssetSettings(true);
 	}
 
-	setAssetSettings(path = [], settings = {}){
+	setAssetSettings(path = [], settings = {}) {
 
 	}
 
-	async getLiveAsset(uuid){
+	async getLiveAsset(uuid) {
 		const projectAsset = await this.getProjectAsset(uuid);
-		if(!projectAsset) return null;
+		if (!projectAsset) return null;
 
 		return await projectAsset.getLiveAsset();
 	}
 
-	getProjectAssetForLiveAsset(liveAsset){
-		//this method doesn't need a loadAssetSettings call because there
-		//is no way to get liveAssets without loading the settings anyway.
-		//So we can keep this method sync.
-		for(const projectAsset of this.projectAssets.values()){
-			if(projectAsset.liveAsset == liveAsset) return projectAsset;
+	getProjectAssetForLiveAsset(liveAsset) {
+		// this method doesn't need a loadAssetSettings call because there
+		// is no way to get liveAssets without loading the settings anyway.
+		// So we can keep this method sync.
+		for (const projectAsset of this.projectAssets.values()) {
+			if (projectAsset.liveAsset == liveAsset) return projectAsset;
 		}
-		for(const projectAsset of this.builtInAssets.values()){
-			if(projectAsset.liveAsset == liveAsset) return projectAsset;
+		for (const projectAsset of this.builtInAssets.values()) {
+			if (projectAsset.liveAsset == liveAsset) return projectAsset;
 		}
 		return null;
 	}
 
-	getAssetUuidFromLiveAsset(liveAsset){
+	getAssetUuidFromLiveAsset(liveAsset) {
 		const projectAsset = this.getProjectAssetForLiveAsset(liveAsset);
-		if(projectAsset){
+		if (projectAsset) {
 			return projectAsset.uuid;
 		}
 		return null;
