c56408ba9c978334cff680c8e8cc4458e9436de3
Jesper van den Ende <jespertheend@gmail.com>
2021-09-20 23:54:36 +0200

    Apply eslint to engineDefines.js

--- src/engineDefines.js
+++ src/engineDefines.js
@@ -1,26 +1,31 @@
-//These are variables used across the engine
-//Changing these defines will cause certain parts of code
-//to get stripped, making the build size smaller
-
-
+/**
+ * @fileoverview These are variables used across the engine.
+ * Changing these defines will cause certain parts of code
+ * to get stripped, making the build size smaller
+ */
 
 /* ==== Generic Defines ==== */
+// These are the defines that are usually needed so they're all true by default.
 
 export const ENABLE_WEBGPU_CLUSTERED_LIGHTS = true;
 
-
-
 /* ======== Editor Defines ======== */
-//These are defines that are generally only needed for the
-//editor. Most builds can set all of these to false.
+// These are defines that are generally only needed for the
+// editor. Most release builds can set all of these to false.
 
-//enables listening for asset changes for assets that are used by the engine
+/**
+ * Enables listening for asset changes for assets that are used by the engine
+ */
 export const ENGINE_ASSETS_LIVE_UPDATES_SUPPORT = true;
 
-//Support for storing default asset link uuids as metadata in entities.
-//This is only needed in the editor since only the editor can handle default asset links.
+/**
+ * Support for storing default asset link uuids as metadata in entities.
+ * This is only needed in the editor since only the editor can handle default asset links.
+ */
 export const DEFAULT_ASSET_LINKS_IN_ENTITY_JSON_EXPORT = true;
 
-//Apply built-in default asset link uuids to the default values for components.
-//These uuids are only available in the editor, asset bundles remove default asset links when bundling.
+/**
+ * Apply built-in default asset link uuids to the default values for components.
+ * These uuids are only available in the editor, asset bundles remove default asset links when bundling.
+ */
 export const EDITOR_DEFAULTS_IN_COMPONENTS = true;
