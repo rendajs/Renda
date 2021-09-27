ddfc4c2a03718fca256f0a6385d7cc752295c5ac
Jesper van den Ende <jespertheend@gmail.com>
2021-09-27 20:58:37 +0200

    Fix eslint issues on DragManager

--- editor/src/Managers/DragManager.js
+++ editor/src/Managers/DragManager.js
@@ -1,21 +1,17 @@
-export default class DragManager{
-	constructor(){
-
-	}
-
+export default class DragManager {
 	createDragFeedbackText({
 		text = "",
-	} = {}){
-		let el = document.createElement("div");
+	} = {}) {
+		const el = document.createElement("div");
 		el.classList.add("dragFeedbackText");
 		el.textContent = text;
 		document.body.appendChild(el);
-		let x = el.offsetWidth / 2;
-		let y = el.offsetHeight / 2;
+		const x = el.offsetWidth / 2;
+		const y = el.offsetHeight / 2;
 		return {el, x, y};
 	}
 
-	removeFeedbackEl(el){
-		if(el.parentElement) el.parentElement.removeChild(el);
+	removeFeedbackEl(el) {
+		if (el.parentElement) el.parentElement.removeChild(el);
 	}
 }
