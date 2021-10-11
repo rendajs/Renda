25d7ebd19fd96400c8af9055ecaaf4813b1de225
Jesper van den Ende <jespertheend@gmail.com>
2021-10-12 00:28:42 +0200

    Eslint fixes for mesh related files

--- src/Core/Mesh.js
+++ src/Core/Mesh.js
@@ -1,8 +1,7 @@
-import Vec3 from "../Math/Vec3.js";
 import MeshAttributeBuffer from "./MeshAttributeBuffer.js";
 
-export default class Mesh{
-	constructor(){
+export default class Mesh {
+	constructor() {
 		this._buffers = [];
 		this._unusedBuffers = new Map();
 		this._vertexState = null;
@@ -15,29 +14,29 @@ export default class Mesh{
 		this.onIndexBufferChangedCbs = new Set();
 	}
 
-	destructor(){
-		for(const buffer of this.getBuffers()){
+	destructor() {
+		for (const buffer of this.getBuffers()) {
 			buffer.destructor();
 		}
 	}
 
-	static get AttributeType(){
+	static get AttributeType() {
 		return {
 			POSITION: 0,
 			NORMAL: 1,
 			COLOR: 2,
-		}
+		};
 	}
 
-	static get IndexFormat(){
+	static get IndexFormat() {
 		return {
 			NONE: 0,
 			UINT_16: 1,
 			UINT_32: 2,
-		}
-	};
+		};
+	}
 
-	static get AttributeFormat(){
+	static get AttributeFormat() {
 		return {
 			INT8: 0,
 			INT16: 1,
@@ -46,11 +45,11 @@ export default class Mesh{
 			FLOAT32: 4,
 			NORM8: 5,
 			NORM16: 6,
-		}
+		};
 	}
 
-	static getByteLengthForAttributeFormat(format){
-		switch(format){
+	static getByteLengthForAttributeFormat(format) {
+		switch (format) {
 			case Mesh.AttributeFormat.INT8:
 				return 1;
 			case Mesh.AttributeFormat.INT16:
@@ -59,89 +58,91 @@ export default class Mesh{
 			case Mesh.AttributeFormat.INT32:
 			case Mesh.AttributeFormat.FLOAT32:
 				return 4;
+			default:
+				throw new Error("Invalid format");
 		}
 	}
 
-	static getBitLengthForAttributeFormat(format){
+	static getBitLengthForAttributeFormat(format) {
 		return Mesh.getByteLengthForAttributeFormat(format) * 8;
 	}
 
-	static getAttributeNameForType(typeId){
-		for(const [name, type] of Object.entries(Mesh.AttributeType)){
-			if(type == typeId) return name;
+	static getAttributeNameForType(typeId) {
+		for (const [name, type] of Object.entries(Mesh.AttributeType)) {
+			if (type == typeId) return name;
 		}
 		return typeId;
 	}
 
-	setIndexData(data){
-		if(data instanceof ArrayBuffer){
-			//data already has the correct format
-			if(this.indexFormat == Mesh.IndexFormat.UINT_16){
+	setIndexData(data) {
+		if (data instanceof ArrayBuffer) {
+			// data already has the correct format
+			if (this.indexFormat == Mesh.IndexFormat.UINT_16) {
 				this.indexLength = data.byteLength / 2;
-			}else if(this.indexFormat == Mesh.IndexFormat.UINT_32){
+			} else if (this.indexFormat == Mesh.IndexFormat.UINT_32) {
 				this.indexLength = data.byteLength / 4;
 			}
-		}else if(ArrayBuffer.isView(data)){
+		} else if (ArrayBuffer.isView(data)) {
 			data = data.buffer.slice(data.byteOffset, data.byteOffset + data.byteLength);
 			this.indexLength = data.length;
-			if(data instanceof Uint32Array){
+			if (data instanceof Uint32Array) {
 				this.indexFormat = Mesh.IndexFormat.UINT_32;
 			}
-		}else if(Array.isArray(data)){
+		} else if (Array.isArray(data)) {
 			this.indexLength = data.length;
 			let bufferLength = 0;
-			if(this.indexFormat == Mesh.IndexFormat.UINT_16){
+			if (this.indexFormat == Mesh.IndexFormat.UINT_16) {
 				bufferLength = data.length * 2;
-			}else if(this.indexFormat == Mesh.IndexFormat.UINT_32){
+			} else if (this.indexFormat == Mesh.IndexFormat.UINT_32) {
 				bufferLength = data.length * 4;
 			}
 			const newBuffer = new ArrayBuffer(bufferLength);
 			const dataView = new DataView(newBuffer);
-			if(this.indexFormat == Mesh.IndexFormat.UINT_16){
-				for(let i=0; i<data.length; i++){
-					dataView.setUint16(i*2, data[i], true);
+			if (this.indexFormat == Mesh.IndexFormat.UINT_16) {
+				for (let i = 0; i < data.length; i++) {
+					dataView.setUint16(i * 2, data[i], true);
 				}
-			}else if(this.indexFormat == Mesh.IndexFormat.UINT_32){
-				for(let i=0; i<data.length; i++){
-					dataView.setUint32(i*4, data[i], true);
+			} else if (this.indexFormat == Mesh.IndexFormat.UINT_32) {
+				for (let i = 0; i < data.length; i++) {
+					dataView.setUint32(i * 4, data[i], true);
 				}
 			}
 			data = newBuffer;
 		}
 
-		if(!(data instanceof ArrayBuffer)){
+		if (!(data instanceof ArrayBuffer)) {
 			throw new TypeError("invalid data type");
 		}
 		this.indexBuffer = data;
 		this.fireIndexBufferChanged();
 	}
 
-	setVertexCount(vertexCount){
+	setVertexCount(vertexCount) {
 		this.vertexCount = vertexCount;
-		for(const buffer of this.getBuffers()){
+		for (const buffer of this.getBuffers()) {
 			buffer.setVertexCount(vertexCount);
 		}
 	}
 
-	setVertexData(attributeType, data, opts){
+	setVertexData(attributeType, data, opts) {
 		const buffer = this.getBufferForAttributeType(attributeType, opts);
-		if(buffer){
+		if (buffer) {
 			buffer.setVertexData(attributeType, data);
 		}
 	}
 
-	setBufferData(attributeBufferOpts){
+	setBufferData(attributeBufferOpts) {
 		const attributeBuffer = new MeshAttributeBuffer(attributeBufferOpts);
 		this.setAttributeBufferData(attributeBuffer);
 	}
 
-	setAttributeBufferData(attributeBuffer){
-		//todo: there's probably still some performance that can be gained here
-		//currently it's decomposing the buffer into vectors and turning
-		//it back into a buffer, if the buffer doesn't need to be changed it
-		//can simply copy or move all the bytes at once
+	setAttributeBufferData(attributeBuffer) {
+		// todo: there's probably still some performance that can be gained here
+		// currently it's decomposing the buffer into vectors and turning
+		// it back into a buffer, if the buffer doesn't need to be changed it
+		// can simply copy or move all the bytes at once
 
-		for(const attribute of attributeBuffer.attributes){
+		for (const attribute of attributeBuffer.attributes) {
 			const arr = Array.from(attributeBuffer.getVertexData(attribute.attributeType));
 			this.setVertexData(attribute.attributeType, arr, {
 				unusedFormat: attribute.format,
@@ -153,53 +154,55 @@ export default class Mesh{
 	getBufferForAttributeType(attributeType, {
 		unusedFormat = Mesh.AttributeFormat.FLOAT32,
 		unusedComponentCount = 3,
-	} = {}){
-		for(const buffer of this.getBuffers()){
-			if(buffer.hasAttributeType(attributeType)){
+	} = {}) {
+		for (const buffer of this.getBuffers()) {
+			if (buffer.hasAttributeType(attributeType)) {
 				return buffer;
 			}
 		}
 
 		const unusedBuffer = new MeshAttributeBuffer({
-			attributes: [{
-				offset: 0,
-				format: unusedFormat,
-				componentCount: unusedComponentCount,
-				attributeType,
-			}],
+			attributes: [
+				{
+					offset: 0,
+					format: unusedFormat,
+					componentCount: unusedComponentCount,
+					attributeType,
+				},
+			],
 			isUnused: true,
 		});
 		unusedBuffer.setVertexCount(this.vertexCount);
-		this._unusedBuffers.set(attributeType, unusedBuffer)
+		this._unusedBuffers.set(attributeType, unusedBuffer);
 		return unusedBuffer;
 	}
 
-	*getBuffers(includeUnused = true){
-		for(const buffer of this._buffers){
+	*getBuffers(includeUnused = true) {
+		for (const buffer of this._buffers) {
 			yield buffer;
 		}
-		if(includeUnused){
-			for(const buffer of this._unusedBuffers.values()){
+		if (includeUnused) {
+			for (const buffer of this._unusedBuffers.values()) {
 				yield buffer;
 			}
 		}
 	}
 
-	get vertexState(){
+	get vertexState() {
 		return this._vertexState;
 	}
 
-	setVertexState(vertexState){
+	setVertexState(vertexState) {
 		this._vertexState = vertexState;
 
 		const oldBuffers = Array.from(this.getBuffers());
 		this._buffers = [];
 		this._unusedBuffers.clear();
 
-		if(vertexState){
-			for(const bufferDescriptor of vertexState.buffers){
+		if (vertexState) {
+			for (const bufferDescriptor of vertexState.buffers) {
 				const attributes = [];
-				for(const attribute of bufferDescriptor.attributes.values()){
+				for (const attribute of bufferDescriptor.attributes.values()) {
 					const attributeType = attribute.attributeType;
 					attributes.push({
 						offset: attribute.offset,
@@ -212,22 +215,22 @@ export default class Mesh{
 					arrayStride: bufferDescriptor.arrayStride,
 					attributes,
 				});
-				if(this.vertexCount) buffer.setVertexCount(this.vertexCount);
+				if (this.vertexCount) buffer.setVertexCount(this.vertexCount);
 				this._buffers.push(buffer);
 			}
 		}
 
-		for(const buffer of oldBuffers){
+		for (const buffer of oldBuffers) {
 			this.setAttributeBufferData(buffer);
 		}
 	}
 
-	onIndexBufferChanged(cb){
+	onIndexBufferChanged(cb) {
 		this.onIndexBufferChangedCbs.add(cb);
 	}
 
-	fireIndexBufferChanged(){
-		for(const cb of this.onIndexBufferChangedCbs){
+	fireIndexBufferChanged() {
+		for (const cb of this.onIndexBufferChangedCbs) {
 			cb();
 		}
 	}
--- src/Core/MeshAttributeBuffer.js
+++ src/Core/MeshAttributeBuffer.js
@@ -2,14 +2,14 @@ import Vec2 from "../Math/Vec2.js";
 import Vec3 from "../Math/Vec3.js";
 import Mesh from "./Mesh.js";
 
-export default class MeshAttributeBuffer{
+export default class MeshAttributeBuffer {
 	constructor({
 		arrayStride = null,
 		attributes = [{offset: 0, format: Mesh.AttributeFormat.FLOAT32, componentCount: 1, attributeType: null}],
 		isUnused = false,
 		arrayBuffer = null,
-	} = {}){
-		if(isUnused && attributes.length != 1){
+	} = {}) {
+		if (isUnused && attributes.length != 1) {
 			throw new Error("Unused attribute buffers must have exactly 1 attribute");
 		}
 		this.arrayStride = null;
@@ -25,51 +25,51 @@ export default class MeshAttributeBuffer{
 		this.onBufferChangedCbs = new Set();
 	}
 
-	destructor(){
-		//todo
+	destructor() {
+		// todo
 	}
 
-	setArrayStride(arrayStride){
-		if(arrayStride != null){
+	setArrayStride(arrayStride) {
+		if (arrayStride != null) {
 			this.arrayStride = arrayStride;
-		}else{
+		} else {
 			this.arrayStride = 0;
-			for(const attribute of this.attributes){
+			for (const attribute of this.attributes) {
 				const neededBytes = attribute.componentCount * Mesh.getByteLengthForAttributeFormat(attribute.format);
 				this.arrayStride = Math.max(this.arrayStride, attribute.offset + neededBytes);
 			}
 		}
 	}
 
-	getDataView(){
-		if(this._currentDataViewBuffer != !this.buffer){
+	getDataView() {
+		if (this._currentDataViewBuffer != !this.buffer) {
 			this._dataView = null;
 		}
-		if(!this._dataView){
+		if (!this._dataView) {
 			this._dataView = new DataView(this.buffer);
 			this._currentDataViewBuffer = this.buffer;
 		}
 		return this._dataView;
 	}
 
-	hasAttributeType(attributeType){
+	hasAttributeType(attributeType) {
 		return !!this.getAttributeSettings(attributeType);
 	}
 
-	getAttributeSettings(attributeType){
-		for(const attribute of this.attributes){
-			if(attribute.attributeType == attributeType){
+	getAttributeSettings(attributeType) {
+		for (const attribute of this.attributes) {
+			if (attribute.attributeType == attributeType) {
 				return attribute;
 			}
 		}
 		return null;
 	}
 
-	setVertexCount(vertexCount){
-		const length = vertexCount*this.arrayStride;
+	setVertexCount(vertexCount) {
+		const length = vertexCount * this.arrayStride;
 		const oldBuffer = this.buffer;
 		this.buffer = new ArrayBuffer(length);
-		if(oldBuffer){
+		if (oldBuffer) {
 			new Uint8Array(this.buffer).set(oldBuffer);
 		}
 
@@ -77,88 +77,88 @@ export default class MeshAttributeBuffer{
 	}
 
 	/**
-		@param {Object} attributeType
-		@param {Object} data
-		@suppress {suspiciousCode}
-	*/
-	setVertexData(attributeType, data){
+	 * @param {Object} attributeType
+	 * @param {Object} data
+	 * @suppress {suspiciousCode}
+	 */
+	setVertexData(attributeType, data) {
 		const attributeSettings = this.getAttributeSettings(attributeType);
 		const dataView = this.getDataView();
 
-		//todo: pick function based on attributeSettings.format
-		let setFunction = dataView.setFloat32.bind(dataView);
-		let valueByteSize = 4;
+		// todo: pick function based on attributeSettings.format
+		const setFunction = dataView.setFloat32.bind(dataView);
+		const valueByteSize = 4;
 
-		if(data instanceof ArrayBuffer){
-			//todo: implement and remove the @suppress for this function
-		}else if(ArrayBuffer.isView(data)){
+		if (data instanceof ArrayBuffer) {
+			// todo: implement and remove the @suppress for this function
+		} else if (ArrayBuffer.isView(data)) {
 			data = data.buffer.slice(data.byteOffset, data.byteOffset + data.byteLength);
-			//todo
-		}else if(Array.isArray(data)){
-			if(data.length <= 0){
+			// todo
+		} else if (Array.isArray(data)) {
+			if (data.length <= 0) {
 				return;
-			}else if(typeof data[0] == "number"){
-				let i=0;
-				while(i<data.length){
-					for(let j=0; j<attributeSettings.componentCount; j++){
-						setFunction(i*this.arrayStride + attributeSettings.offset + valueByteSize * j, data[i], true);
+			} else if (typeof data[0] == "number") {
+				let i = 0;
+				while (i < data.length) {
+					for (let j = 0; j < attributeSettings.componentCount; j++) {
+						setFunction(i * this.arrayStride + attributeSettings.offset + valueByteSize * j, data[i], true);
 					}
 					i++;
 				}
-			}else if(data[0] instanceof Vec2){
-				if(attributeSettings.componentCount != 2){
+			} else if (data[0] instanceof Vec2) {
+				if (attributeSettings.componentCount != 2) {
 					throw new TypeError("Vec2 array expected");
 				}
-				for(const [i, pos] of data.entries()){
-					setFunction(i*this.arrayStride + attributeSettings.offset + valueByteSize * 0, pos.x, true);
-					setFunction(i*this.arrayStride + attributeSettings.offset + valueByteSize * 1, pos.y, true);
+				for (const [i, pos] of data.entries()) {
+					setFunction(i * this.arrayStride + attributeSettings.offset + valueByteSize * 0, pos.x, true);
+					setFunction(i * this.arrayStride + attributeSettings.offset + Number(valueByteSize), pos.y, true);
 				}
-			}else if(data[0] instanceof Vec3){
-				if(attributeSettings.componentCount != 3){
+			} else if (data[0] instanceof Vec3) {
+				if (attributeSettings.componentCount != 3) {
 					throw new TypeError("Vec3 array expected");
 				}
-				for(const [i, pos] of data.entries()){
-					setFunction(i*this.arrayStride + attributeSettings.offset + valueByteSize * 0, pos.x, true);
-					setFunction(i*this.arrayStride + attributeSettings.offset + valueByteSize * 1, pos.y, true);
-					setFunction(i*this.arrayStride + attributeSettings.offset + valueByteSize * 2, pos.z, true);
+				for (const [i, pos] of data.entries()) {
+					setFunction(i * this.arrayStride + attributeSettings.offset + valueByteSize * 0, pos.x, true);
+					setFunction(i * this.arrayStride + attributeSettings.offset + Number(valueByteSize), pos.y, true);
+					setFunction(i * this.arrayStride + attributeSettings.offset + valueByteSize * 2, pos.z, true);
 				}
 			}
-			//todo: support more vector types
-		}else{
+			// todo: support more vector types
+		} else {
 			throw new TypeError("invalid data type");
 		}
 
 		this.fireBufferChanged();
 	}
 
-	*getVertexData(attributeType){
+	*getVertexData(attributeType) {
 		const attributeSettings = this.getAttributeSettings(attributeType);
-		if(!attributeSettings) return;
+		if (!attributeSettings) return;
 
 		const dataView = this.getDataView();
 
-		//todo: pick function based on attributeSettings.format
-		let getFunction = dataView.getFloat32.bind(dataView);
-		let valueByteSize = 4;
-		//todo, add cases for different component counts
-		if(attributeSettings.componentCount == 3){
-			let i=0;
-			while(i <= this.buffer.byteLength - this.arrayStride){
+		// todo: pick function based on attributeSettings.format
+		const getFunction = dataView.getFloat32.bind(dataView);
+		const valueByteSize = 4;
+		// todo, add cases for different component counts
+		if (attributeSettings.componentCount == 3) {
+			let i = 0;
+			while (i <= this.buffer.byteLength - this.arrayStride) {
 				const x = getFunction(i + attributeSettings.offset + valueByteSize * 0, true);
-				const y = getFunction(i + attributeSettings.offset + valueByteSize * 1, true);
+				const y = getFunction(i + attributeSettings.offset + Number(valueByteSize), true);
 				const z = getFunction(i + attributeSettings.offset + valueByteSize * 2, true);
-				yield new Vec3(x,y,z);
+				yield new Vec3(x, y, z);
 				i += this.arrayStride;
 			}
 		}
 	}
 
-	onBufferChanged(cb){
+	onBufferChanged(cb) {
 		this.onBufferChangedCbs.add(cb);
 	}
 
-	fireBufferChanged(){
-		for(const cb of this.onBufferChangedCbs){
+	fireBufferChanged() {
+		for (const cb of this.onBufferChangedCbs) {
 			cb();
 		}
 	}
