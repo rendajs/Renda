5684fd2dd7279c9a3b00a2001fb7b7a47dbe9e40
Jesper van den Ende <jespertheend@gmail.com>
2021-10-28 21:49:17 +0200

    Ignore circular dependency warnings when building engine

--- build.js
+++ build.js
@@ -13,6 +13,10 @@ import jscc from "rollup-plugin-jscc";
 				},
 			}),
 		],
+		onwarn: message => {
+			if (message.code == "CIRCULAR_DEPENDENCY") return;
+			console.error(message.message);
+		},
 	});
 	await bundle.write({
 		file: "build/game-engine.js",
