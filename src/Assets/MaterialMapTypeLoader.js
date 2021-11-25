1bae8eef9f972fc3b7d2e8cccc3fbcc893f3e9ba
Jesper van den Ende <jespertheend@gmail.com>
2021-11-25 22:34:04 +0100

    Add jsdoc to MaterialMapTypeLoader

--- src/Assets/MaterialMapTypeLoader.js
+++ src/Assets/MaterialMapTypeLoader.js
@@ -1,5 +1,10 @@
 import {MaterialMapTypeSettings} from "../Rendering/MaterialMapTypeSettings.js";
 
+/**
+ * Base class for MaterialMapType loaders.
+ * Register the constructor with AssetLoaderTypeMaterialMap.registerMaterialMapTypeLoader()
+ * to add support for loading MaterialMapTypes.
+ */
 export class MaterialMapTypeLoader {
 	// this should return the same uuid as the typeUuid of your MaterialMapType
 	static get typeUuid() {
