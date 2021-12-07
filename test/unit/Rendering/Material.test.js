cf585ec472abf23ccca7a07593a0e6c781488987
Jesper van den Ende <jespertheend@gmail.com>
2021-12-08 00:19:42 +0100

    Add more material unit tests

--- test/unit/Rendering/Material.test.js
+++ test/unit/Rendering/Material.test.js
@@ -8,4 +8,30 @@ test.describe("A new Material", () => {
 		const done = material.getAllProperties().next().done;
 		expect(done).toBe(true);
 	});
+
+	test("should call the destructor callbacks", () => {
+		const material = new Material();
+		let destructorCalled = false;
+		const cb = () => {
+			destructorCalled = true;
+		};
+
+		material.onDestructor(cb);
+		material.destructor();
+
+		expect(destructorCalled).toBe(true);
+	});
+
+	test("should not call removed destructors.", () => {
+		const material = new Material();
+		let destructorCalled = false;
+		const cb = () => {
+			destructorCalled = true;
+		};
+
+		material.onDestructor(cb);
+		material.removeOnDestructor(cb);
+
+		expect(destructorCalled).toBe(false);
+	});
 });
