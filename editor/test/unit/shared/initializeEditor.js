4d3bd07a600951958f0daffd10235684872ba5e9
Jesper van den Ende <jespertheend@gmail.com>
2021-12-15 23:18:00 +0100

    chore(test) add utility for importing circular references

---
+++ editor/test/unit/shared/initializeEditor.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview Utility for initializing the editor.
+ * When importing certain files from the editor, errors can occur when this
+ * causes files with circular references to be loaded in a certain order.
+ *
+ * Import this file before any other files from the editor.
+ */
+
+import "../../../src/editorInstance.js";
