e83e00726d4f35f7f850afb12fde067e54255c05
Jesper van den Ende <jespertheend@gmail.com>
2021-12-21 16:50:05 +0100

    chore: Make some more tests work in Deno

---
+++ test/unit/src/rendering/Material.test.js
@@ -0,0 +1,35 @@
+import {assertEquals} from "https://deno.land/std@0.118.0/testing/asserts.ts";
+import {Material} from "../../../../src/Rendering/Material.js";
+
+Deno.test("Empty getAllProperties() for new materials", () => {
+	const material = new Material();
+
+	const done = material.getAllProperties().next().done;
+	assertEquals(done, true);
+});
+
+Deno.test("should call the destructor callbacks", () => {
+	const material = new Material();
+	let destructorCalled = false;
+	const cb = () => {
+		destructorCalled = true;
+	};
+
+	material.onDestructor(cb);
+	material.destructor();
+
+	assertEquals(destructorCalled, true);
+});
+
+Deno.test("should not call removed destructors.", () => {
+	const material = new Material();
+	let destructorCalled = false;
+	const cb = () => {
+		destructorCalled = true;
+	};
+
+	material.onDestructor(cb);
+	material.removeOnDestructor(cb);
+
+	assertEquals(destructorCalled, false);
+});
