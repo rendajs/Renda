5b1c23e77534a025a18672474afa10a03d767f87
Jesper van den Ende <jespertheend@gmail.com>
2021-12-22 17:41:35 +0100

    fix: Use the engine util script for the editor console

--- editor/src/main.js
+++ editor/src/main.js
@@ -6,8 +6,11 @@ const projectSelector = getProjectSelectorInstance();
 globalThis["projectSelector"] = projectSelector;
 
 (async () => {
-	const module = await import("./Util/Util.js");
-	window["Util"] = module;
+	// We'll assign some modules to the global scope so that they can be used in the browser console.
+	// This allows for using `Util.generateUuid()` for instance.
+	const module = await import("../../src/util/mod.js");
+	globalThis["Util"] = module;
+	// todo: add more modules here? Math types etc. could be useful.
 })();
 
 (async () => {
