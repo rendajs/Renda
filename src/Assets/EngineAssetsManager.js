e5c55636c0cd2fb8b73f8f5b814fd7bb5e7ee281
Jesper van den Ende <jespertheend@gmail.com>
2021-12-22 21:58:29 +0100

    chore: Add types and docs to EngineAssetsManager

--- src/Assets/EngineAssetsManager.js
+++ src/Assets/EngineAssetsManager.js
@@ -1,18 +1,38 @@
 import {ENGINE_ASSETS_LIVE_UPDATES_SUPPORT} from "../engineDefines.js";
 
+/** @typedef {Parameters<import("./AssetLoader.js").AssetLoader["getAsset"]>} GetAssetArgs */
+/** @typedef {(...args: GetAssetArgs) => any} GetEngineAssetHandler */
+/** @typedef {(asset: any) => any} WatchAssetCallback */
+
+/**
+ * This class is responsible for loading assets that are being used by the
+ * engine itself. This class mostly exists so that we can reload assets when
+ * they change during development of the editor.
+ * When this code runs in release builds, `ENGINE_ASSETS_LIVE_UPDATES_SUPPORT`
+ * is expected to be false. When that is the case this class functions as little
+ * more than a wrapper around the `AssetLoader.getAsset()` method.
+ * But when this is not the case, {@linkcode addGetAssetHandler} callbacks are
+ * called for every {@linkcode getAsset} request. This allows you to hook into
+ * the asset loading logic and load assets from a `BuiltInAssetManager`.
+ */
 export class EngineAssetsManager {
 	/**
 	 * @param {import("./AssetLoader.js").AssetLoader} assetLoader
 	 */
 	constructor(assetLoader) {
-		if (!ENGINE_ASSETS_LIVE_UPDATES_SUPPORT) return;
 		this.assetLoader = assetLoader;
+		if (!ENGINE_ASSETS_LIVE_UPDATES_SUPPORT) return;
 
+		/** @type {Set<GetEngineAssetHandler>} */
 		this.getAssetHandlers = new Set();
 
-		this.watchingAssetCbs = new Map(); // <uuid, Set<cb>>
+		/** @type {Map<import("../mod.js").UuidString, Set<WatchAssetCallback>>} */
+		this.watchingAssetCbs = new Map();
 	}
 
+	/**
+	 * @param {GetAssetArgs} args
+	 */
 	async getAsset(...args) {
 		if (ENGINE_ASSETS_LIVE_UPDATES_SUPPORT) {
 			for (const handler of this.getAssetHandlers) {
@@ -23,6 +43,10 @@ export class EngineAssetsManager {
 		return await this.assetLoader.getAsset(...args);
 	}
 
+	/**
+	 * @param {import("../util/mod.js").UuidString} uuid
+	 * @param {WatchAssetCallback} onAssetChangeCb
+	 */
 	async watchAsset(uuid, onAssetChangeCb) {
 		const asset = await this.getAsset(uuid);
 		onAssetChangeCb(asset);
@@ -36,11 +60,21 @@ export class EngineAssetsManager {
 		}
 	}
 
+	/**
+	 * Adds a handler that gets called every time an engine asset is requested.
+	 * The handler should return a live asset that will then be used by whatever
+	 * engine part of the engine requested it.
+	 * @param {GetEngineAssetHandler} handlerFunction
+	 */
 	addGetAssetHandler(handlerFunction) {
 		if (!ENGINE_ASSETS_LIVE_UPDATES_SUPPORT) return;
 		this.getAssetHandlers.add(handlerFunction);
 	}
 
+	/**
+	 * Reloads any currently loaded assets that were loaded using {@linkcode watchAsset}.
+	 * @param {import("../mod.js").UuidString} uuid
+	 */
 	async notifyAssetChanged(uuid) {
 		if (!ENGINE_ASSETS_LIVE_UPDATES_SUPPORT) return;
 		const cbs = this.watchingAssetCbs.get(uuid);
