5e537c96d68ed8745c1eeb83d7e9074549851a54
Jesper van den Ende <jespertheend@gmail.com>
2022-01-15 21:50:18 +0100

    chore: Download d.ts files for url imports in dev.js script

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,10 +1,16 @@
-#!/usr/bin/env -S deno run --allow-run --allow-read --allow-write
+#!/usr/bin/env -S deno run --allow-run --allow-read --allow-write --allow-net
 
 import {createHash} from "https://deno.land/std@0.118.0/hash/mod.ts";
 import {setCwd} from "https://deno.land/x/chdir_anywhere@v0.0.2/mod.js";
 setCwd();
 
 const HASH_STORAGE_PATH = "./lastDevInitHash.txt";
+const DOWNLOAD_TYPE_URLS = [
+	"https://esm.sh/rollup@2.61.1",
+	"https://unpkg.com/rollup-plugin-cleanup@3.2.1/index.d.ts",
+	"https://esm.sh/rollup-plugin-jscc@2.0.0",
+];
+
 const thisScriptContent = await Deno.readFile("./dev.js");
 const hash = createHash("md5");
 hash.update(Deno.version.deno);
@@ -28,13 +34,56 @@ if (currentHash != previousHash || Deno.args.includes("--force-fti")) {
 		cmd: ["deno", "types"],
 		stdout: "piped",
 	});
-
 	const typesContent = await getDenoTypesProcess.output();
 	await Deno.mkdir("../.denoTypes/@types/deno-types/", {recursive: true});
 	await Deno.writeFile("../.denoTypes/@types/deno-types/index.d.ts", typesContent);
 
+	/**
+	 * @param {string} url
+	 * @param {string} dtsContent
+	 */
+	async function createDts(url, dtsContent) {
+		const urlObj = new URL(url);
+		let fileName = urlObj.pathname;
+		if (fileName.startsWith("/")) {
+			fileName = fileName.substring(1);
+		}
+		fileName = fileName.replaceAll("/", "_");
+		if (!fileName.endsWith(".d.ts")) {
+			fileName += ".d.ts";
+		}
+		await Deno.writeTextFile(`../.denoTypes/urlImports/${fileName}`, dtsContent);
+		console.log(`Created ${fileName}`);
+	}
+
+	await Deno.mkdir("../.denoTypes/urlImports/", {recursive: true});
+	/** @type {Promise<void>[]} */
+	const typeFetchPromises = [];
+	for (const url of DOWNLOAD_TYPE_URLS) {
+		const promise = (async () => {
+			console.log(`Fetching types for "${url}"`);
+			const tsResponse = await fetch(url);
+			if (tsResponse.ok) {
+				if (url.endsWith(".d.ts")) {
+					await createDts(url, await tsResponse.text());
+				} else {
+					const typesUrl = tsResponse.headers.get("x-typescript-types");
+					if (typesUrl) {
+						const dtsResponse = await fetch(typesUrl);
+						await createDts(url, await dtsResponse.text());
+					}
+				}
+			}
+		})();
+		typeFetchPromises.push(promise);
+	}
+
+	await Promise.all(typeFetchPromises);
+
 	console.log("First time set up done.");
 }
+
+Deno.exit();
 
 Deno.chdir("..");
 
