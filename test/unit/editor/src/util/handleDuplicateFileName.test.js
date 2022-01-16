dcfbf99c537f39e0dca0b54a3c18f420588bc10d
Jesper van den Ende <jespertheend@gmail.com>
2022-01-16 16:41:21 +0100

    chore: Add tests for handleDuplicateFileName

---
+++ test/unit/editor/src/util/handleDuplicateFileName.test.js
@@ -0,0 +1,51 @@
+import {assertEquals} from "https://deno.land/std@0.118.0/testing/asserts.ts";
+import {handleDuplicateFileName} from "../../../../../editor/src/Util/Util.js";
+
+Deno.test("No existing", () => {
+	const result = handleDuplicateFileName([], "existingFile", ".txt");
+
+	assertEquals(result, "existingFile.txt");
+});
+
+Deno.test("One existing", () => {
+	const existingFiles = ["existingFile.txt"];
+
+	const result = handleDuplicateFileName(existingFiles, "existingFile", ".txt");
+
+	assertEquals(result, "existingFile 1.txt");
+});
+
+Deno.test("Multiple existing", () => {
+	const existingFiles = [
+		"existingFile.txt",
+		"existingFile 1.txt",
+		"existingFile 2.txt",
+		// 3 is missing
+		"existingFile 4.txt",
+	];
+
+	const result = handleDuplicateFileName(existingFiles, "existingFile", ".txt");
+
+	assertEquals(result, "existingFile 3.txt");
+});
+
+Deno.test("Custom numberPrefix", () => {
+	const existingFiles = ["existingFile.txt"];
+	const numberPrefix = "-";
+
+	const result = handleDuplicateFileName(existingFiles, "existingFile", ".txt", numberPrefix);
+
+	assertEquals(result, "existingFile-1.txt");
+});
+
+Deno.test("Using EditorFileSystemReadDirResult", () => {
+	/** @type {import("../../../../../editor/src/Util/FileSystems/EditorFileSystem.js").EditorFileSystemReadDirResult} */
+	const readDirResult = {
+		directories: [],
+		files: ["existingFile.txt"],
+	};
+
+	const result = handleDuplicateFileName(readDirResult, "existingFile", ".txt");
+
+	assertEquals(result, "existingFile 1.txt");
+});
