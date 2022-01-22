375e42295a58560b36b269b8bdc8f2d5e8ef4c07
Jesper van den Ende <jespertheend@gmail.com>
2022-01-22 16:26:30 +0100

    chore: Add support for applying fake imports map to coverage data

--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,26 @@
 #!/usr/bin/env -S deno run --no-check --allow-run --allow-read --allow-write --allow-env
 
+import {join} from "https://deno.land/std@0.119.0/path/mod.ts";
 import {setCwd} from "https://deno.land/x/chdir_anywhere@v0.0.2/mod.js";
 setCwd();
 
 Deno.chdir("..");
+
+const DENO_COVERAGE_DIR = ".coverage/denoCoverage";
+const FAKE_IMPORTS_COVERAGE_DIR = ".coverage/fakeImportsCoverageMap";
+
+/**
+ * @param {string} path
+ */
+async function removeMaybeDirectory(path) {
+	try {
+		await Deno.remove(path, {recursive: true});
+	} catch (e) {
+		if (!(e instanceof Deno.errors.NotFound)) {
+			throw e;
+		}
+	}
+}
 
 const filteredArgs = ["--coverage", "--html"];
 
@@ -18,29 +35,36 @@ if (needsHtmlCoverageReport) {
 	needsCoverage = true;
 }
 
-const testCommand = ["deno", "test", "--no-check", "--allow-read", "--unstable", ...extraArgs];
+const denoTestArgs = ["deno", "test", "--no-check", "--allow-read", "--unstable"];
+const applicationArgs = [];
 if (needsCoverage) {
-	try {
-		await Deno.remove(".coverage/denoCoverage", {recursive: true});
-	} catch (e) {
-		if (!(e instanceof Deno.errors.NotFound)) {
-			throw e;
-		}
-	}
-	testCommand.push("--coverage=.coverage/denoCoverage");
+	await removeMaybeDirectory(DENO_COVERAGE_DIR);
+	await removeMaybeDirectory(FAKE_IMPORTS_COVERAGE_DIR);
+	denoTestArgs.push("--allow-write");
+	denoTestArgs.push(`--coverage=${DENO_COVERAGE_DIR}`);
+	const coverageMapPath = join(Deno.cwd(), FAKE_IMPORTS_COVERAGE_DIR);
+	applicationArgs.push(`--fi-coverage-map=${coverageMapPath}`);
 }
-const testProcess = Deno.run({
-	cmd: testCommand,
-});
+denoTestArgs.push(...extraArgs);
+const cmd = [...denoTestArgs, "--", ...applicationArgs];
+
+console.log(`Running: ${cmd.join(" ")}`);
+const testProcess = Deno.run({cmd});
 const testStatus = await testProcess.status();
 if (!testStatus.success) {
 	Deno.exit(testStatus.code);
 }
 
 if (needsCoverage) {
+	console.log("Applying fake-imports coverage map.");
+	const p = Deno.run({
+		cmd: ["deno", "run", "--allow-read", "--no-check", "--allow-write", "https://raw.githubusercontent.com/jespertheend/fake-imports/main/applyCoverage.js", FAKE_IMPORTS_COVERAGE_DIR, DENO_COVERAGE_DIR],
+	});
+	await p.status();
+
 	console.log("Generating lcov.info...");
 	const coverageProcess = Deno.run({
-		cmd: ["deno", "coverage", ".coverage/denoCoverage", "--lcov", "--exclude=test/unit"],
+		cmd: ["deno", "coverage", DENO_COVERAGE_DIR, "--lcov",  "--exclude=test/unit"],
 		stdout: "piped",
 	});
 	const lcov = await coverageProcess.output();
@@ -51,6 +75,7 @@ if (needsCoverage) {
 	await Deno.writeFile(".coverage/lcov.info", lcov);
 
 	if (needsHtmlCoverageReport) {
+		console.log("Generating HTML coverage report...");
 		let genHtmlProcess = null;
 		try {
 			genHtmlProcess = Deno.run({
