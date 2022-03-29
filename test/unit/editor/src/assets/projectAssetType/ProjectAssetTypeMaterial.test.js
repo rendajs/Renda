93e1e9fc494237eed4c3222090f0dc080765fb5c
Jesper van den Ende <jespertheend@gmail.com>
2022-03-30 00:28:28 +0200

    test(assets): Add more tests for ProjectAssetTypeMaterial

--- test/unit/editor/src/assets/projectAssetType/ProjectAssetTypeMaterial.test.js
+++ test/unit/editor/src/assets/projectAssetType/ProjectAssetTypeMaterial.test.js
@@ -1,14 +1,30 @@
 import "../../../shared/initializeEditor.js";
-
 import {assertEquals, assertExists} from "asserts";
 import {ProjectAssetTypeMaterial} from "../../../../../../editor/src/assets/projectAssetType/ProjectAssetTypeMaterial.js";
 import {createMockDependencies} from "./shared.js";
+import {Material} from "../../../../../../src/mod.js";
+import {MaterialMap} from "../../../../../../src/rendering/MaterialMap.js";
+
+const BASIC_MATERIAL_MAP_UUID = "basic material map uuid";
+
+function basicSetup() {
+	const {projectAssetTypeArgs} = createMockDependencies({
+		getAssetUuidFromLiveAssetImpl: liveAsset => {
+			if (liveAsset instanceof MaterialMap) {
+				return BASIC_MATERIAL_MAP_UUID;
+			}
+			return null;
+		},
+	});
+	const projectAssetType = new ProjectAssetTypeMaterial(...projectAssetTypeArgs);
+
+	return {projectAssetType};
+}
 
 Deno.test({
 	name: "Creating a new asset",
 	async fn() {
-		const {projectAssetTypeArgs} = createMockDependencies();
-		const projectAssetType = new ProjectAssetTypeMaterial(...projectAssetTypeArgs);
+		const {projectAssetType} = basicSetup();
 		const {liveAsset, editorData} = await projectAssetType.createNewLiveAssetData();
 		const assetData = await projectAssetType.saveLiveAssetData(liveAsset || null, editorData || null);
 
@@ -19,11 +35,49 @@ Deno.test({
 Deno.test({
 	name: "getLiveAssetData() with no material map",
 	async fn() {
-		const {projectAssetTypeArgs} = createMockDependencies();
-		const projectAssetType = new ProjectAssetTypeMaterial(...projectAssetTypeArgs);
+		const {projectAssetType} = basicSetup();
 		const liveAssetData = await projectAssetType.getLiveAssetData(null);
 
 		assertExists(liveAssetData.liveAsset);
 		assertEquals(liveAssetData.liveAsset.materialMap, null);
+	},
+});
+
+Deno.test({
+	name: "saveLiveAssetData() with no live asset",
+	async fn() {
+		const {projectAssetType} = basicSetup();
+
+		const assetData = await projectAssetType.saveLiveAssetData(null, null);
+
+		assertEquals(assetData, {});
+	},
+});
+
+Deno.test({
+	name: "saveLiveAssetData() with no material map",
+	async fn() {
+		const {projectAssetType} = basicSetup();
+
+		const material = new Material();
+		const assetData = await projectAssetType.saveLiveAssetData(material, null);
+
+		assertEquals(assetData, {});
+	},
+});
+
+Deno.test({
+	name: "saveLiveAssetData() with a material map",
+	async fn() {
+		const {projectAssetType} = basicSetup();
+
+		const material = new Material();
+		const materialMap = new MaterialMap();
+		material.setMaterialMap(materialMap);
+		const assetData = await projectAssetType.saveLiveAssetData(material, null);
+
+		assertEquals(assetData, {
+			map: BASIC_MATERIAL_MAP_UUID,
+		});
 	},
 });
--- test/unit/editor/src/assets/projectAssetType/shared.js
+++ test/unit/editor/src/assets/projectAssetType/shared.js
@@ -1,7 +1,20 @@
-export function createMockDependencies() {
+/**
+ * @param {Object} [options]
+ * @param {(liveAsset: Object | null) => import("../../../../../../src/mod.js").UuidString | null} [options.getAssetUuidFromLiveAssetImpl]
+ */
+export function createMockDependencies({
+	getAssetUuidFromLiveAssetImpl = () => null,
+} = {}) {
 	const editor = /** @type {import("../../../../../../editor/src/Editor.js").Editor} */ ({});
+
 	const projectAsset = /** @type {import("../../../../../../editor/src/assets/ProjectAsset.js").ProjectAsset<any>} */ ({});
-	const assetManager = /** @type {import("../../../../../../editor/src/assets/AssetManager.js").AssetManager} */ ({});
+
+	const assetManager = /** @type {import("../../../../../../editor/src/assets/AssetManager.js").AssetManager} */ ({
+		getAssetUuidFromLiveAsset(liveAsset) {
+			return getAssetUuidFromLiveAssetImpl(liveAsset);
+		},
+	});
+
 	const assetTypeManager = /** @type {import("../../../../../../editor/src/assets/ProjectAssetTypeManager.js").ProjectAssetTypeManager} */ ({});
 
 	const projectAssetTypeArgs = /** @type {const} */ ([
