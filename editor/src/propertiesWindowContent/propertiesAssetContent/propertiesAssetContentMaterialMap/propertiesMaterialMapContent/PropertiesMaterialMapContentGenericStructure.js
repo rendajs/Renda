7f9459656060b8bd70f084f402af6995ca482da3
Jesper van den Ende <jespertheend@gmail.com>
2022-04-19 20:20:01 +0200

    refactor(materials): Remove type reference to webgpu material maps

--- editor/src/propertiesWindowContent/propertiesAssetContent/propertiesAssetContentMaterialMap/propertiesMaterialMapContent/PropertiesMaterialMapContentGenericStructure.js
+++ editor/src/propertiesWindowContent/propertiesAssetContent/propertiesAssetContentMaterialMap/propertiesMaterialMapContent/PropertiesMaterialMapContentGenericStructure.js
@@ -42,7 +42,7 @@ export class PropertiesMaterialMapContentGenericStructure extends PropertiesMate
 	}
 
 	/**
-	 * @param {import("../../../../assets/projectAssetType/projectAssetTypeMaterialMap/materialMapTypeSerializers/WebGpuMaterialMapTypeSerializer.js").WebGpuMaterialMapTypeDiskData} customData
+	 * @param {*} customData
 	 * @override
 	 */
 	async customAssetDataFromLoad(customData) {
