faf21d531fd49ec107e24ffca06ecbf03669501a
Jesper van den Ende <jespertheend@gmail.com>
2022-07-29 23:31:06 +0200

    fix: Resolve some case sensitive imports

--- src/gizmos/mod.js
+++ src/gizmos/mod.js
@@ -4,6 +4,6 @@ export {Gizmo} from "./gizmos/Gizmo.js";
 export {CameraClusterDataGizmo} from "./gizmos/CameraClusterDataGizmo.js";
 export {CameraGizmo} from "./gizmos/CameraGizmo.js";
 export {CameraIconGizmo} from "./gizmos/iconGizmos/CameraIconGizmo.js";
-export {IconGizmo} from "./gizmos/IconGizmos/IconGizmo.js";
+export {IconGizmo} from "./gizmos/iconGizmos/IconGizmo.js";
 export {LightIconGizmo} from "./gizmos/iconGizmos/LightIconGizmo.js";
 export {TranslationGizmo} from "./gizmos/TranslationGizmo.js";
