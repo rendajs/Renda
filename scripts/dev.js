e3d050a042e9ad44c349557366f4149afdc59422
Jesper van den Ende <jespertheend@users.noreply.github.com>
2022-08-11 21:16:56 +0200

    chore: Upgrade-tsc-helper (#21)
    * add this branch to ci
    
    * upgrade tsc helper and add cacheHashFile option
    
    * upgrade to 0.0.12
    
    * Use cacheHashFile for ci deno types cache
    
    * Remove this branch from ci events
    
    * Rename some stuff
    
    * Rename more stuff

--- scripts/dev.js
+++ scripts/dev.js
@@ -10,13 +10,14 @@
 
 import {setCwd} from "chdir-anywhere";
 import {DevServer} from "./DevServer.js";
-import {generateTypes, preCollectImports} from "https://deno.land/x/deno_tsc_helper@v0.0.10/mod.js";
+import {createCacheHashFile, generateTypes} from "https://deno.land/x/deno_tsc_helper@v0.0.12/mod.js";
 import {downloadNpmPackages} from "https://deno.land/x/npm_devinit@v0.0.2/mod.ts";
 
-/** @type {import("https://deno.land/x/deno_tsc_helper@v0.0.10/mod.js").GenerateTypesOptions} */
+/** @type {import("https://deno.land/x/deno_tsc_helper@v0.0.12/mod.js").GenerateTypesOptions} */
 const generateTypesOptions = {
 	outputDir: "../.denoTypes",
 	importMap: "../importmap.json",
+	cacheHashFile: "cacheHashfile",
 	preCollectedImportsFile: "precollectedImports.json",
 	include: [
 		".",
@@ -32,8 +33,8 @@ const generateTypesOptions = {
 	},
 };
 
-if (Deno.args.includes("--precollect-imports")) {
-	await preCollectImports(generateTypesOptions);
+if (Deno.args.includes("--create-cache-hashfile")) {
+	await createCacheHashFile(generateTypesOptions);
 	Deno.exit();
 }
 
