ff03394413bf66824f4d16b86fed1deff8f2ac2e
Jesper van den Ende <jespertheend@gmail.com>
2022-08-14 00:31:31 +0200

    fix tests

--- test/unit/editor/src/tasks/shared/basicWorker.js
+++ test/unit/editor/src/tasks/shared/basicWorker.js
@@ -5,7 +5,7 @@ import {TypedMessenger} from "../../../../../../src/util/TypedMessenger.js";
 /** @type {TypedMessenger<{}, BasicWorkerResponseHandlers>} */
 const messenger = new TypedMessenger();
 messenger.setSendHandler(data => {
-	globalThis.postMessage(data);
+	globalThis.postMessage(data.sendData);
 });
 globalThis.addEventListener("message", e => {
 	messenger.handleReceivedMessage(e.data);
