16e87d2f9a2fe4b9476e793d0cc466819e05a50d
Jesper van den Ende <jespertheend@gmail.com>
2022-08-21 01:18:58 +0200

    Support for running dependency tasks when requesting generated file assets

--- test/unit/editor/src/tasks/TaskManager.test.js
+++ test/unit/editor/src/tasks/TaskManager.test.js
@@ -5,9 +5,7 @@ import {assertSpyCall, assertSpyCalls, spy} from "std/testing/mock.ts";
 import {TypedMessenger} from "../../../../../src/util/TypedMessenger.js";
 import {injectMockEditorInstance} from "../../../../../editor/src/editorInstance.js";
 import {createMockProjectAsset} from "../assets/shared/createMockProjectAsset.js";
-
-const BASIC_UUID = "BASIC_UUID";
-const BASIC_UUID_2 = "BASIC_UUID_2";
+import {stringArrayEquals} from "../../../../../src/mod.js";
 
 Deno.test({
 	name: "init(), registers the default task types",
@@ -39,11 +37,9 @@ Deno.test({
 		const manager = new TaskManager();
 		class Task1 extends Task {
 			static type = "namespace:type1";
-			static typeUuid = BASIC_UUID;
 		}
 		class Task2 extends Task {
 			static type = "namespace:type2";
-			static typeUuid = BASIC_UUID_2;
 		}
 		manager.registerTaskType(Task1);
 		manager.registerTaskType(Task2);
@@ -91,7 +87,6 @@ Deno.test({
 		for (const typeStr of wrongTypes) {
 			class ExtendedTask extends Task {
 				static type = typeStr;
-				static typeUuid = BASIC_UUID;
 			}
 
 			assertThrows(() => {
@@ -101,31 +96,59 @@ Deno.test({
 	},
 });
 
+/**
+ * @param {Object} options
+ * @param {{path: import("../../../../../editor/src/util/fileSystems/EditorFileSystem.js").EditorFileSystemPath, projectAsset: import("../../../../../editor/src/assets/ProjectAsset.js").ProjectAssetAny}[]} [options.pathProjectAssets]
+ * @param {Map<import("../../../../../src/mod.js").UuidString, import("../../../../../editor/src/assets/ProjectAsset.js").ProjectAssetAny>} [options.uuidProjectAssets]
+ */
+function basicTaskRunningSetup({
+	pathProjectAssets = [],
+	uuidProjectAssets = new Map(),
+} = {}) {
+	/**
+	 * @typedef RegisteredAssetData
+	 * @property {import("std/testing/mock.ts").Spy<import("../../../../../editor/src/assets/ProjectAsset.js").ProjectAssetAny, [fileData: unknown], Promise<void>>} writeAssetDataSpy
+	 */
+	/** @type {RegisteredAssetData[]} */
+	const registeredAssets = [];
+	injectMockEditorInstance(/** @type {import("../../../../../editor/src/Editor.js").Editor} */ ({
+		projectManager: {
+			assetManager: {
+				async getProjectAssetFromPath(path, options) {
+					for (const {path: path2, projectAsset} of pathProjectAssets) {
+						if (stringArrayEquals(path, path2)) {
+							return projectAsset;
+						}
+					}
+					return null;
+				},
+				async getProjectAssetFromUuid(uuid, options) {
+					if (!uuid) return null;
+					return uuidProjectAssets.get(uuid) || null;
+				},
+				async registerAsset(path, assetType) {
+					const {projectAsset} = createMockProjectAsset();
+					registeredAssets.push({
+						writeAssetDataSpy: spy(projectAsset, "writeAssetData"),
+					});
+					return projectAsset;
+				},
+			},
+		},
+	}));
+
+	return {
+		registeredAssets,
+		cleanup() {
+			injectMockEditorInstance(null);
+		},
+	};
+}
+
 Deno.test({
 	name: "running a task",
 	async fn() {
-		/**
-		 * @typedef RegisteredAssetData
-		 * @property {import("std/testing/mock.ts").Spy<import("../../../../../editor/src/assets/ProjectAsset.js").ProjectAssetAny, [fileData: unknown], Promise<void>>} writeAssetDataSpy
-		 */
-		/** @type {RegisteredAssetData[]} */
-		const registeredAssets = [];
-		injectMockEditorInstance(/** @type {import("../../../../../editor/src/Editor.js").Editor} */ ({
-			projectManager: {
-				assetManager: {
-					async getProjectAssetFromPath(path, options) {
-						return null;
-					},
-					async registerAsset(path, assetType) {
-						const {projectAsset} = createMockProjectAsset();
-						registeredAssets.push({
-							writeAssetDataSpy: spy(projectAsset, "writeAssetData"),
-						});
-						return projectAsset;
-					},
-				},
-			},
-		}));
+		const {registeredAssets, cleanup} = basicTaskRunningSetup();
 
 		try {
 			const manager = new TaskManager();
@@ -133,7 +156,6 @@ Deno.test({
 			/** @extends {Task<{}>} */
 			class ExtendedTask extends Task {
 				static type = "namespace:type";
-				static typeUuid = BASIC_UUID;
 				static workerUrl = new URL("./shared/basicWorker.js", import.meta.url);
 
 				/** @type {TypedMessenger<import("./shared/basicWorker.js").BasicWorkerResponseHandlers, {}>} */
@@ -172,15 +194,17 @@ Deno.test({
 
 			const runTaskSpy = spy(ExtendedTask.prototype, "runTask");
 
-			manager.registerTaskType(ExtendedTask);
-			await manager.runTask({
-				taskFileContent: {
+			const {projectAsset: taskProjectAsset} = createMockProjectAsset({
+				readAssetDataReturnValue: {
 					taskType: "namespace:type",
 					taskConfig: {
 						foo: "bar",
 					},
 				},
 			});
+
+			manager.registerTaskType(ExtendedTask);
+			await manager.runTask(taskProjectAsset);
 
 			assertSpyCalls(runTaskSpy, 1);
 			assertEquals(runTaskSpy.calls[0].args[0].config, {
@@ -199,7 +223,130 @@ Deno.test({
 			assertInstanceOf(taskInstance, ExtendedTask);
 			taskInstance.worker.terminate();
 		} finally {
-			injectMockEditorInstance(null);
+			cleanup();
+		}
+	},
+});
+
+Deno.test({
+	name: "running a task with a dependency task",
+	async fn() {
+		const DEPENDENDCY_PATH = ["path", "to", "dependency.txt"];
+		const TOUCHED_ASSET_UUID = "TOUCHED_ASSET_UUID";
+
+		const {projectAsset: dependencyProjectAsset1} = createMockProjectAsset();
+		const {projectAsset: dependencyProjectAsset2} = createMockProjectAsset();
+
+		/** @type {Map<import("../../../../../src/mod.js").UuidString, import("../../../../../editor/src/assets/ProjectAsset.js").ProjectAssetAny>} */
+		const uuidProjectAssets = new Map();
+		uuidProjectAssets.set(TOUCHED_ASSET_UUID, dependencyProjectAsset2);
+		const {cleanup} = basicTaskRunningSetup({
+			pathProjectAssets: [
+				{
+					path: DEPENDENDCY_PATH,
+					projectAsset: dependencyProjectAsset1,
+				},
+			],
+			uuidProjectAssets,
+		});
+
+		try {
+			const manager = new TaskManager();
+
+			let dependencyRunCount = 0;
+
+			/** @extends {Task<{}>} */
+			class DependencyTask extends Task {
+				static type = "namespace:dependency";
+
+				/**
+				 * @param {import("../../../../../editor/src/tasks/task/Task.js").RunTaskOptions<{}>} options
+				 */
+				async runTask(options) {
+					dependencyRunCount++;
+					/** @type {import("../../../../../editor/src/tasks/task/Task.js").RunTaskReturn} */
+					const returnValue = {
+						writeAssets: [
+							{
+								path: DEPENDENDCY_PATH,
+								assetType: "namespace:type",
+								fileData: "foo",
+							},
+						],
+						touchedAssets: [TOUCHED_ASSET_UUID],
+					};
+					return returnValue;
+				}
+			}
+			manager.registerTaskType(DependencyTask);
+
+			/**
+			 * @typedef ParentTaskConfig
+			 * @property {import("../../../../../editor/src/util/fileSystems/EditorFileSystem.js").EditorFileSystemPath} [assetPath]
+			 * @property {import("../../../../../src/mod.js").UuidString} [assetUuid]
+			 */
+
+			/** @extends {Task<ParentTaskConfig>} */
+			class ParentTask extends Task {
+				static type = "namespace:parent";
+
+				/**
+				 * @param {import("../../../../../editor/src/tasks/task/Task.js").RunTaskOptions<ParentTaskConfig>} options
+				 */
+				async runTask(options) {
+					if (options.config.assetPath) {
+						await options.readAssetFromPath(DEPENDENDCY_PATH, {});
+					} else if (options.config.assetUuid) {
+						await options.readAssetFromUuid(options.config.assetUuid, {});
+					}
+					return {};
+				}
+			}
+			manager.registerTaskType(ParentTask);
+
+			const {projectAsset: dependencyTaskAsset} = createMockProjectAsset({
+				readAssetDataReturnValue: {
+					taskType: "namespace:dependency",
+					taskConfig: {},
+				},
+			});
+
+			const {projectAsset: parentTaskAsset1} = createMockProjectAsset({
+				readAssetDataReturnValue: {
+					taskType: "namespace:parent",
+					/** @type {ParentTaskConfig} */
+					taskConfig: {
+						assetPath: DEPENDENDCY_PATH,
+					},
+				},
+			});
+
+			const {projectAsset: parentTaskAsset2} = createMockProjectAsset({
+				readAssetDataReturnValue: {
+					taskType: "namespace:parent",
+					/** @type {ParentTaskConfig} */
+					taskConfig: {
+						assetUuid: TOUCHED_ASSET_UUID,
+					},
+				},
+			});
+
+			// First we run the dependency to let the manager know dependency.txt is created by this task.
+			await manager.runTask(dependencyTaskAsset);
+
+			assertEquals(dependencyRunCount, 1);
+
+			// Then we run the first parent task, which should run the dependency task again because DEPENDENDCY_PATH was written.
+			await manager.runTask(parentTaskAsset1);
+
+			assertEquals(dependencyRunCount, 2);
+
+			// Then we run the second parent task, which should run the dependency task a third time because TOUCHED_ASSET_UUID was touched.
+			await manager.runTask(parentTaskAsset2);
+
+			assertEquals(dependencyRunCount, 3);
+		} finally {
+			cleanup();
 		}
 	},
 });
