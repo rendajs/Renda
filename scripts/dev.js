885c794333e467866e4d1f5ec1eda68f48f4f541
Jesper van den Ende <jespertheend@gmail.com>
2022-09-08 20:45:01 +0200

    Exclude puppeteer from vendored types

--- scripts/dev.js
+++ scripts/dev.js
@@ -25,7 +25,10 @@ const generateTypesOptions = {
 		"../editor/devSocket",
 		"../editor/scripts",
 	],
-	excludeUrls: ["rollup-plugin-commonjs"],
+	excludeUrls: [
+		"rollup-plugin-commonjs",
+		"puppeteer",
+	],
 	extraTypeRoots: {
 		// We prefix webgpu with aa to ensure it is placed above deno-types.
 		// The Deno types include webgpu types but they are outdated.
