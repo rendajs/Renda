44b54f12a0262d8d939465f3233cd0f66337bb37
Jesper van den Ende <jespertheend@gmail.com>
2022-09-08 20:57:31 +0200

    Remove closure compiler import

--- editor/devSocket/src/ClosureCompilerManager.js
+++ editor/devSocket/src/ClosureCompilerManager.js
@@ -1,9 +1,13 @@
-import {createRequire} from "std/node/module.ts";
+// TODO: I'll leave all of this here for now, but at some point we might want
+// to either remove this and only write tests to check if closure builds are
+// still working, or we want to keep this and convert this to a "npm:google-closure-compiler" import.
 
-const compatRequire = createRequire(import.meta.url);
-const closureCompiler = compatRequire("google-closure-compiler");
+// import {createRequire} from "std/node/module.ts";
 
-const ClosureCompiler = closureCompiler.compiler;
+// const compatRequire = createRequire(import.meta.url);
+// const closureCompiler = compatRequire("google-closure-compiler");
+
+// const ClosureCompiler = closureCompiler.compiler;
 
 export class ClosureCompilerManager {
 	/**
@@ -17,28 +21,29 @@ export class ClosureCompilerManager {
 		externs = [],
 		args = {},
 	}) {
-		const compiler = new ClosureCompiler(args);
-		const result = await new Promise(r => {
-			/**
-			 * @param {number} exitCode
-			 * @param {string} stdOut
-			 * @param {string} stdErr
-			 */
-			const procCallback = (exitCode, stdOut, stdErr) => {
-				r({exitCode, stdOut, stdErr});
-			};
-			const proc = compiler.run(procCallback);
-			if (!proc.stdin) {
-				throw new Error("Assertion failed: Closeure compiler process has no stdin.");
-			}
-			proc.stdin.on("error", () => {
-				// this callback exists to prevent node from throwing an error
-				// when the compiler fails before stdin is parsed
-				// errors will be handled from the compiler.run() callback
-			});
-			proc.stdin.write(JSON.stringify(inputFiles));
-		});
+		throw new Error("Closure builds are not supported at the moment");
+		// const compiler = new ClosureCompiler(args);
+		// const result = await new Promise(r => {
+		// 	/**
+		// 	 * @param {number} exitCode
+		// 	 * @param {string} stdOut
+		// 	 * @param {string} stdErr
+		// 	 */
+		// 	const procCallback = (exitCode, stdOut, stdErr) => {
+		// 		r({exitCode, stdOut, stdErr});
+		// 	};
+		// 	const proc = compiler.run(procCallback);
+		// 	if (!proc.stdin) {
+		// 		throw new Error("Assertion failed: Closeure compiler process has no stdin.");
+		// 	}
+		// 	proc.stdin.on("error", () => {
+		// 		// this callback exists to prevent node from throwing an error
+		// 		// when the compiler fails before stdin is parsed
+		// 		// errors will be handled from the compiler.run() callback
+		// 	});
+		// 	proc.stdin.write(JSON.stringify(inputFiles));
+		// });
 
-		return result;
+		// return result;
 	}
 }
