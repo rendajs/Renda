2b69b5db2d16f7bea1f82c917a2981316723ced8
Jesper van den Ende <jespertheend@gmail.com>
2022-09-18 23:11:47 +0200

    Better test

--- test/unit/src/core/Entity/transformations.test.js
+++ test/unit/src/core/Entity/transformations.test.js
@@ -1,4 +1,5 @@
 import {assertEquals} from "std/testing/asserts.ts";
+import {stub} from "std/testing/mock.ts";
 import {Entity, Mat4, Quat, Vec3} from "../../../../../src/mod.js";
 import {assertMatAlmostEquals, assertVecAlmostEquals} from "../../../shared/asserts.js";
 
@@ -483,19 +484,33 @@ Deno.test({
 
 Deno.test({
 	name: "Issue #132: Stackoverflow when getting world matrix",
+	only: true,
 	fn() {
-		const entity = new Entity({
-			matrix: new Mat4([
-				[0.23185248938019934, 7.831232006951038e-16, 0.9727509563877509, 0],
-				[5.952468856531243e-16, 0.9999999999989068, -9.469357669400846e-16, 0],
-				[-0.9727509563873817, 7.985763921546837e-16, 0.23185248938011135, 0],
-				[4.63563934158383, -3.7913191723642954e-15, -1.1048917651091632, 1],
-			]),
+		// When passing the result from Mat4.compose() back in to Mat4.decompose()
+		// the values might not always be exactly the same, probably due to
+		// floating point errors.
+		// To simulate this behaviour we make Mat4.decompose return a different
+		// value every time it is called.
+		// When all components of pos/rot/scale are changed, the entity simply
+		// replaces the entire vec3/quat. But if one of the components stay
+		// the same, a special path is taken where the entity tries to only
+		// update the changed components. This was where issue #132 used to happen.
+		// That is why we only increment the first component of the decompose result.
+		let i = 0;
+		const decomposeStub = stub(Mat4.prototype, "decompose", () => {
+			return {
+				pos: new Vec3(i++, 0, 0),
+				rot: new Quat(i++, 0, 0, 1),
+				scale: new Vec3(i++, 1, 1),
+			};
 		});
-		entity.worldPos.set(4.63563934158383, -3.7913191723642954e-15, -1.1048917651091632);
-		entity.worldRot.set(0.7848096869247758, -5.56030392249633e-16, -0.6197368435949532, 5.984774979834381e-17);
-		entity.worldScale.set(0.9999999999925395, 0.9999999999989068, 0.99999999999216);
-		/* eslint-disable-next-line no-unused-expressions */
-		entity.worldMatrix;
+
+		try {
+			const entity = new Entity();
+			entity.worldPos.set(1, 2, 3);
+			entity.worldMatrix;
+		} finally {
+			decomposeStub.restore();
+		}
 	},
 });
