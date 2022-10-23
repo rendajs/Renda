7d298fbabe7bef8df7d5cff367eeb9a1429d545d
Jesper van den Ende <jespertheend@gmail.com>
2022-10-23 17:25:25 +0200

    Only lint modified files

--- scripts/lint.js
+++ scripts/lint.js
@@ -85,7 +85,58 @@ if (useIo) {
 	}
 	await writeStdout(results[0].output);
 } else {
-	const results = await eslint.lintFiles("**/*.js");
+	let lintFilesStr = "";
+	if (Deno.args.includes("--all")) {
+		lintFilesStr = "**/*.js";
+	} else {
+		const proc = Deno.run({
+			cmd: ["git", "status", "-z"],
+			stdout: "piped",
+		});
+		const status = await proc.status();
+		if (!status.success) {
+			throw new Error("Failed to determine which files were changed, run with --all to lint all files in the repository.");
+		}
+		const result = await proc.output();
+
+		let files = [];
+		let i = 0;
+		let prevStart = 0;
+		let skipNext = false;
+		const textDecoder = new TextDecoder();
+		while (i < result.byteLength) {
+			if (result[i] == 0) {
+				const chunk = result.slice(prevStart, i);
+				prevStart = i + 1;
+				const text = textDecoder.decode(chunk);
+				if (skipNext) {
+					skipNext = false;
+				} else {
+					const xy = text.slice(0, 2);
+					const path = text.slice(3);
+					if (["M", "A", "C", "R", "U", "?"].includes(xy[1])) {
+						files.push(path);
+					} else if (["M ", "A "].includes(xy)) {
+						files.push(path);
+					} else if (xy[0] == "R") {
+						skipNext = true;
+						files.push(path);
+					}
+				}
+			}
+			i++;
+		}
+		files = files.filter(f => f.endsWith(".js"));
+		if (files.length == 0) {
+			console.log("No files have been modified, there's nothing to lint. Use --all if you wish to forcefully lint all files.");
+			Deno.exit();
+		} else if (files.length == 1) {
+			lintFilesStr = files[0];
+		} else {
+			lintFilesStr = "{" + files.join(",") + "}";
+		}
+	}
+	const results = await eslint.lintFiles(lintFilesStr);
 	if (fix) {
 		await ESLint.outputFixes(results);
 	}
