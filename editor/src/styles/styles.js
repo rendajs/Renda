2b381ea2497afbdb88d1810b32c1440fe75b38fe
Jesper van den Ende <jespertheend@gmail.com>
2022-11-14 00:57:23 +0100

    split styles in several files

--- editor/src/styles/styles.js
+++ editor/src/styles/styles.js
@@ -1,6 +1,10 @@
 // @ts-ignore
 import mainSheet from "./styles.css" assert {type: "css"};
+// @ts-ignore
+import variables from "./variables.css" assert {type: "css"};
+// @ts-ignore
+import zIndex from "./zIndex.css" assert {type: "css"};
 
 document.adoptedStyleSheets = [
-	mainSheet,
+	mainSheet, variables, zIndex,
 ];
