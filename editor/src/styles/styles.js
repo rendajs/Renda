f458ac266868efc754c302871bd0c2e49e97f37a
Jesper van den Ende <jespertheend@gmail.com>
2022-11-20 15:58:52 +0100

    split more styles

--- editor/src/styles/styles.js
+++ editor/src/styles/styles.js
@@ -1,10 +1,31 @@
+// Eslint isn't able to handle import assertion syntax yet. So for now we will
+// combine all imports in this file. Perhaps in the future we might be able to
+// import styles from files related to the elements using the styles.
+
 // @ts-ignore
 import mainSheet from "./styles.css" assert {type: "css"};
 // @ts-ignore
 import variables from "./variables.css" assert {type: "css"};
 // @ts-ignore
 import zIndex from "./zIndex.css" assert {type: "css"};
+// @ts-ignore
+import projectSelector from "../projectSelector/projectSelector.css" assert {type: "css"};
+// @ts-ignore
+import windowManagement from "../windowManagement/windowManagement.css" assert {type: "css"};
+// @ts-ignore
+import contextMenu from "../ui/contextMenus/contextMenu.css" assert {type: "css"};
+// @ts-ignore
+import guis from "../ui/guis.css" assert {type: "css"};
+// @ts-ignore
+import treeView from "../ui/treeView.css" assert {type: "css"};
 
 document.adoptedStyleSheets = [
-	mainSheet, variables, zIndex,
+	mainSheet,
+	variables,
+	zIndex,
+	projectSelector,
+	windowManagement,
+	contextMenu,
+	guis,
+	treeView,
 ];
