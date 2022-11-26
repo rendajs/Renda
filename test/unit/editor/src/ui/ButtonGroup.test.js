33d63be7d65ba7295c89b06582684b4b944c62c5
Jesper van den Ende <jespertheend@gmail.com>
2022-11-26 20:37:29 +0100

    Fix button group visible child classes not being set correctly

--- test/unit/editor/src/ui/ButtonGroup.test.js
+++ test/unit/editor/src/ui/ButtonGroup.test.js
@@ -23,29 +23,38 @@ Deno.test({
 			 * @param {[boolean, boolean, boolean]} containsState
 			 */
 			function testClasses(className, containsState) {
-				assertEquals(button1.el.classList.contains(className), containsState[0]);
-				assertEquals(button2.el.classList.contains(className), containsState[1]);
-				assertEquals(button3.el.classList.contains(className), containsState[2]);
+				assertEquals(button1.el.classList.contains(className), containsState[0], `Expected button 1 to ${containsState[0] ? "" : "not "}contain "${className}"`);
+				assertEquals(button2.el.classList.contains(className), containsState[1], `Expected button 2 to ${containsState[0] ? "" : "not "}contain "${className}"`);
+				assertEquals(button3.el.classList.contains(className), containsState[2], `Expected button 3 to ${containsState[0] ? "" : "not "}contain "${className}"`);
 			}
 
+			testClasses("hidden", [false, false, false]);
 			testClasses("first-visible-child", [true, false, false]);
 			testClasses("last-visible-child", [false, false, true]);
 
 			button1.setVisibility(false);
+			testClasses("hidden", [true, false, false]);
 			testClasses("first-visible-child", [false, true, false]);
 			testClasses("last-visible-child", [false, false, true]);
 
 			button3.setVisibility(false);
+			testClasses("hidden", [true, false, true]);
 			testClasses("first-visible-child", [false, true, false]);
 			testClasses("last-visible-child", [false, true, false]);
 
 			button2.setVisibility(false);
+			testClasses("hidden", [true, true, true]);
 			testClasses("first-visible-child", [false, false, false]);
 			testClasses("last-visible-child", [false, false, false]);
 
 			button1.setVisibility(true);
+			testClasses("hidden", [false, true, true]);
+			testClasses("first-visible-child", [true, false, false]);
+			testClasses("last-visible-child", [true, false, false]);
+
 			button2.setVisibility(true);
 			button3.setVisibility(true);
+			testClasses("hidden", [false, false, false]);
 			testClasses("first-visible-child", [true, false, false]);
 			testClasses("last-visible-child", [false, false, true]);
 
