b7d194519bdbcca36ab760110096eb1b5208b0e6
Jesper van den Ende <jespertheend@gmail.com>
2023-02-05 14:59:24 +0100

    Revert "feat: Add proxy to TypedMessenger (#369)"
    This reverts commit 707cdbada37b9b1a88c86d6861e727d15f5f3f01.

--- src/util/TypedMessenger.js
+++ src/util/TypedMessenger.js
@@ -95,12 +95,6 @@
  */
 
 /**
- * @template {TypedMessengerSignatures} TReq
- * @template {boolean} TRequireHandlerReturnObjects
- * @typedef {{[x in keyof TReq]: (...args: Parameters<TReq[x]>) => Promise<GetReturnType<TReq, x, TRequireHandlerReturnObjects>>}} TypedMessengerProxy
- */
-
-/**
  * @typedef RequestHandlerReturn
  * @property {any} returnValue
  * @property {Transferable[]} [transfer]
@@ -195,40 +189,6 @@ export class TypedMessenger {
 
 		/** @private @type {Map<number, Set<(message: TypedMessengerResponseMessageSendData<TReq, keyof TReq, TRequireHandlerReturnObjects>) => void>>} */
 		this.onRequestIdMessageCbs = new Map();
-
-		const proxy = new Proxy({
-		}, {
-			get: (target, prop, receiver) => {
-				if (typeof prop == "symbol") {
-					return undefined;
-				}
-				/**
-				 * @param {Parameters<TReq[string]>} args
-				 */
-				return async (...args) => {
-					return await this.send(prop, ...args);
-				};
-			},
-		});
-		/**
-		 * The proxy property allows you to call functions by their actual name,
-		 * rather than passing them as a string parameter.
-		 *
-		 * ## Example
-		 *
-		 * Instead of
-		 * ```js
-		 * const result = await messenger.send("myFunction", 1, 2, 3);
-		 * ```
-		 * You can do
-		 * ```js
-		 * const result = await messenger.proxy.myFunction(1, 2, 3);
-		 * ```
-		 *
-		 * The difference might seem subtle, but the biggest benefit is that you can jump
-		 * to the implementation of a function by using the 'go to definition' option in your IDE.
-		 */
-		this.proxy = /** @type {TypedMessengerProxy<TReq, TRequireHandlerReturnObjects>} */ (proxy);
 	}
 
 	/**
