fabe0ceba585c10d76ba8617f7d729e4ed7e334c
Jesper van den Ende <jespertheend@gmail.com>
2023-02-27 00:50:07 +0100

    test: Fix broken NumericGui unit tests

--- test/unit/studio/src/ui/NumericGui.test.js
+++ test/unit/studio/src/ui/NumericGui.test.js
@@ -1,37 +1,39 @@
 import {assertEquals} from "std/testing/asserts.ts";
+import {stub} from "std/testing/mock.ts";
 import {NumericGui} from "../../../../../studio/src/ui/NumericGui.js";
+import { runWithDom } from "../../shared/runWithDom.js";
+import {WheelEvent} from "fake-dom/FakeWheelEvent.js";
+import {MouseEvent} from "fake-dom/FakeMouseEvent.js";
 
 Deno.test({
 	name: "Hide cursor when scrolling",
-	ignore: true,
-	sanitizeOps: false,
-	sanitizeResources: false,
 	fn: () => {
-		// const window = initializeDom();
-		const numericGui = new NumericGui();
+		runWithDom(() => {
+			const numericGui = new NumericGui();
+			stub(numericGui.el, "blur");
 
-		const wheelEvent = new window.WheelEvent("wheel", {deltaY: 1});
-		numericGui.el.dispatchEvent(wheelEvent);
+			const wheelEvent = new WheelEvent("wheel", {deltaY: 1});
+			numericGui.el.dispatchEvent(wheelEvent);
 
-		assertEquals(numericGui.el.classList.contains("no-cursor"), true);
+			assertEquals(numericGui.el.classList.contains("no-cursor"), true);
+		})
 	},
 });
 
 Deno.test({
 	name: "Show the cursor when moving mouse after scrolling",
-	ignore: true,
-	sanitizeOps: false,
-	sanitizeResources: false,
 	fn: () => {
-		// const window = initializeDom();
-		const numericGui = new NumericGui();
+		runWithDom(() => {
+			const numericGui = new NumericGui();
+			stub(numericGui.el, "blur");
 
-		const wheelEvent = new window.WheelEvent("wheel", {deltaY: 1});
-		numericGui.el.dispatchEvent(wheelEvent);
-		const mouseEvent = new window.MouseEvent("mousemove");
-		numericGui.el.dispatchEvent(mouseEvent);
+			const wheelEvent = new WheelEvent("wheel", {deltaY: 1});
+			numericGui.el.dispatchEvent(wheelEvent);
+			const mouseEvent = new MouseEvent("mousemove");
+			numericGui.el.dispatchEvent(mouseEvent);
 
-		assertEquals(numericGui.el.classList.contains("no-cursor"), false);
+			assertEquals(numericGui.el.classList.contains("no-cursor"), false);
+		})
 	},
 });
 
