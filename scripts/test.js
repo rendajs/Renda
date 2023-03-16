341f2a9430b7f96183f16702c75e3a24aab58e27
Jesper van den Ende <jespertheend@users.noreply.github.com>
2023-03-16 22:55:43 +0100

    chore: Move fetched types to jsdelivr (#451)

--- scripts/test.js
+++ scripts/test.js
@@ -160,7 +160,7 @@ if (needsCoverage) {
 	if (coverageMapExists) {
 		console.log("Applying fake-imports coverage map.");
 		const p = Deno.run({
-			cmd: ["deno", "run", "--allow-read", "--no-check", "--allow-write", "https://deno.land/x/fake_imports@v0.6.0/applyCoverageMap.js", FAKE_IMPORTS_COVERAGE_DIR, DENO_COVERAGE_DIR],
+			cmd: ["deno", "run", "--allow-read", "--no-check", "--allow-write", "https://deno.land/x/fake_imports@v0.8.1/applyCoverageMap.js", FAKE_IMPORTS_COVERAGE_DIR, DENO_COVERAGE_DIR],
 		});
 		await p.status();
 	}
