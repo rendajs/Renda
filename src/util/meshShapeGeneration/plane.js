9a99b680391efc594db6090c251217b8774629a4
Jesper van den Ende <jespertheend@gmail.com>
2023-03-20 00:15:11 +0100

    Add tests for createPlane

--- src/util/meshShapeGeneration/plane.js
+++ src/util/meshShapeGeneration/plane.js
@@ -1,4 +1,6 @@
-import { Mesh, Vec2, Vec3 } from "../../mod.js";
+import {Vec2} from "../../math/Vec2.js";
+import {Vec3} from "../../math/Vec3.js";
+import {Mesh} from "../../core/Mesh.js";
 
 /**
  * @param {object} options
@@ -14,47 +16,47 @@ export function createPlane({
 	height = 1,
 	widthSegments = 1,
 	heightSegments = 1,
-}) {
-		/** @type {Vec3[]} */
-		const positions = [];
-		/** @type {Vec3[]} */
-		const normals = [];
-		/** @type {Vec2[]} */
-		const uvs = [];
-		/** @type {number[]} */
-		const indices = [];
-		/** @type {Vec3[]} */
-		const tangents = [];
-		/** @type {Vec3[]} */
-		const bitangents = [];
+} = {}) {
+	/** @type {Vec3[]} */
+	const positions = [];
+	/** @type {Vec3[]} */
+	const normals = [];
+	/** @type {Vec2[]} */
+	const uvs = [];
+	/** @type {number[]} */
+	const indices = [];
+	/** @type {Vec3[]} */
+	const tangents = [];
+	/** @type {Vec3[]} */
+	const bitangents = [];
 
-		for (let x=0; x<=widthSegments; x++) {
-			for (let y=0; y<=heightSegments; y++) {
-				let u = x/widthSegments;
-				let v = y/heightSegments;
+	for (let x = 0; x <= widthSegments; x++) {
+		for (let y = 0; y <= heightSegments; y++) {
+			const u = x / widthSegments;
+			const v = y / heightSegments;
 
-				const xPos = (u - 0.5) * width;
-				const yPos = (v - 0.5) * height;
-				const pos = new Vec3(xPos,0,yPos);
-				positions.push(pos);
+			const xPos = (u - 0.5) * width;
+			const yPos = (v - 0.5) * height;
+			const pos = new Vec3(xPos, 0, yPos);
+			positions.push(pos);
 
-				normals.push(new Vec3(0, 1, 0));
-				uvs.push(new Vec2(u,v));
-				tangents.push(new Vec3(1, 0, 0));
-				bitangents.push(new Vec3(0, 0, -1));
-			}
+			normals.push(new Vec3(0, 1, 0));
+			uvs.push(new Vec2(u, v));
+			tangents.push(new Vec3(1, 0, 0));
+			bitangents.push(new Vec3(0, 0, -1));
 		}
+	}
 
-		indices.push(0, 1, 2, 1, 3, 2);
+	indices.push(0, 1, 2, 1, 3, 2);
 
-		const mesh = new Mesh();
-		mesh.setVertexCount(positions.length);
-		if (vertexState) mesh.setVertexState(vertexState);
-		mesh.setIndexData(indices);
-		mesh.setVertexData(Mesh.AttributeType.POSITION, positions, {unusedFormat: Mesh.AttributeFormat.FLOAT32, unusedComponentCount: 3});
-		mesh.setVertexData(Mesh.AttributeType.NORMAL, normals, {unusedFormat: Mesh.AttributeFormat.FLOAT32, unusedComponentCount: 3});
-		mesh.setVertexData(Mesh.AttributeType.UV1, uvs, {unusedFormat: Mesh.AttributeFormat.FLOAT32, unusedComponentCount: 2});
-		mesh.setVertexData(Mesh.AttributeType.TANGENT, tangents, {unusedFormat: Mesh.AttributeFormat.FLOAT32, unusedComponentCount: 3});
-		mesh.setVertexData(Mesh.AttributeType.BITANGENT, bitangents, {unusedFormat: Mesh.AttributeFormat.FLOAT32, unusedComponentCount: 3});
-		return mesh;
+	const mesh = new Mesh();
+	mesh.setVertexCount(positions.length);
+	if (vertexState) mesh.setVertexState(vertexState);
+	mesh.setIndexData(indices);
+	mesh.setVertexData(Mesh.AttributeType.POSITION, positions, {unusedFormat: Mesh.AttributeFormat.FLOAT32, unusedComponentCount: 3});
+	mesh.setVertexData(Mesh.AttributeType.NORMAL, normals, {unusedFormat: Mesh.AttributeFormat.FLOAT32, unusedComponentCount: 3});
+	mesh.setVertexData(Mesh.AttributeType.UV1, uvs, {unusedFormat: Mesh.AttributeFormat.FLOAT32, unusedComponentCount: 2});
+	mesh.setVertexData(Mesh.AttributeType.TANGENT, tangents, {unusedFormat: Mesh.AttributeFormat.FLOAT32, unusedComponentCount: 3});
+	mesh.setVertexData(Mesh.AttributeType.BITANGENT, bitangents, {unusedFormat: Mesh.AttributeFormat.FLOAT32, unusedComponentCount: 3});
+	return mesh;
 }
---
+++ test/unit/src/util/meshShapeGeneration/plane.test.js
@@ -0,0 +1,56 @@
+import {assertEquals, assertInstanceOf} from "std/testing/asserts.ts";
+import {Vec3, createPlane} from "../../../../../src/mod.js";
+import {assertVecAlmostEquals} from "../../../shared/asserts.js";
+
+Deno.test({
+	name: "Basic plane",
+	fn() {
+		const mesh = createPlane();
+
+		const positionBuffer = mesh.getBufferForAttributeType(0);
+		const vertexData = Array.from(positionBuffer.getVertexData(0));
+		assertEquals(vertexData.length, 4);
+
+		const indexData = Array.from(mesh.getIndexData());
+		assertEquals(indexData, [0, 1, 2, 1, 3, 2]);
+	},
+});
+
+Deno.test({
+	name: "Custom segments",
+	fn() {
+		const mesh = createPlane({
+			widthSegments: 5,
+			heightSegments: 3,
+		});
+
+		const positionBuffer = mesh.getBufferForAttributeType(0);
+		const vertexData = Array.from(positionBuffer.getVertexData(0));
+		assertEquals(vertexData.length, 24);
+	},
+});
+
+Deno.test({
+	name: "Custom size",
+	fn() {
+		const mesh = createPlane({
+			width: 5,
+			height: 3,
+		});
+
+		const positionBuffer = mesh.getBufferForAttributeType(0);
+		const min = new Vec3();
+		const max = new Vec3();
+		for (const vert of positionBuffer.getVertexData(0)) {
+			assertInstanceOf(vert, Vec3);
+			min.x = Math.min(min.x, vert.x);
+			min.y = Math.min(min.y, vert.y);
+			min.z = Math.min(min.z, vert.z);
+			max.x = Math.max(max.x, vert.x);
+			max.y = Math.max(max.y, vert.y);
+			max.z = Math.max(max.z, vert.z);
+		}
+		assertVecAlmostEquals(min, [-2.5, 0, -1.5]);
+		assertVecAlmostEquals(max, [2.5, 0, 1.5]);
+	},
+});
