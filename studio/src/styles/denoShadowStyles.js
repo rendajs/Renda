1ebef1b6002c557ac41d79169e06a7a12c508025
Jesper van den Ende <jespertheend@gmail.com>
2023-03-19 17:02:45 +0100

    fix tests

--- studio/src/styles/denoShadowStyles.js
+++
@@ -1,7 +0,0 @@
-/**
- * @fileoverview Deno doesn't support importing css files and will cause errors in unit tests.
- * Since unit tests don't need styles, we've added an entry to the deno import map so that it
- * imports this file instead of shadowStyles.js
- */
-
-export const contentWindowHistorySheet = {};
