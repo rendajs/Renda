0e9858fb77e2c19b9550075109f5f5c33642d4b6
Jesper van den Ende <jespertheend@gmail.com>
2023-03-22 23:34:34 +0100

    better default position for entity editor orbit

--- studio/src/preferences/autoRegisterPreferences.js
+++ studio/src/preferences/autoRegisterPreferences.js
@@ -15,9 +15,18 @@ const autoRegisterPreferences = /** @type {const} */ ({
 	"entityEditor.invertScrollOrbitY": pref({
 		type: "boolean",
 	}),
-	"entityEditor.orbitLookPos": pref({type: "unknown"}),
-	"entityEditor.orbitLookRot": pref({type: "unknown"}),
-	"entityEditor.orbitLookDist": pref({type: "number"}),
+	"entityEditor.orbitLookPos": pref({
+		type: "unknown",
+		default: [0, 0, 0],
+	}),
+	"entityEditor.orbitLookRot": pref({
+		type: "unknown",
+		default: [0.13806283196906857, 0.37838630992789435, -0.057187497461225936, 0.9135053612442318],
+	}),
+	"entityEditor.orbitLookDist": pref({
+		type: "number",
+		default: 0,
+	}),
 	"entityEditor.loadedEntityPath": pref({type: "unknown"}),
 });
 
