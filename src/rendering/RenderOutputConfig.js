ff1f02832bacae8c0d6c006297299fcaa46dbdbc
Jesper van den Ende <jespertheend@gmail.com>
2023-04-06 01:42:53 +0200

    Use outputformat of domtarget for pipelines

--- src/rendering/RenderOutputConfig.js
+++ src/rendering/RenderOutputConfig.js
@@ -3,7 +3,6 @@ export class RenderOutputConfig {
 	 * @param {object} options
 	 * @param {GPUTextureFormat} [options.depthStencilFormat]
 	 * @param {number} [options.multisampleCount]
-	 * @param {Iterable<GPUColorTargetState>} [options.fragmentTargets]
 	 */
 	constructor({
 		depthStencilFormat = "depth24plus",
