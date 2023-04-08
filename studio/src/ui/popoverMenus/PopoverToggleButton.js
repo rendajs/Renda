5b9cbc276c16c4ad40b9651abe0ec061c78ffaa7
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-08 17:41:41 +0100

    improve callbacks

--- studio/src/ui/popoverMenus/PopoverToggleButton.js
+++ studio/src/ui/popoverMenus/PopoverToggleButton.js
@@ -17,11 +17,32 @@ export class PopoverToggleButton extends Button {
 	#popoverManager;
 
 	/**
+	 * @typedef {(popover: T) => void} onPopoverCreatedCallback
+	 * @type {Set<onPopoverCreatedCallback>}
+	 */
+	#onPopoverCreatedCbs = new Set();
+
+	/**
 	 * @param {new (...args: any[]) => T} PopoverConstructor
 	 * @param {import("./PopoverManager.js").PopoverManager} popoverManager
 	 * @param {import("../Button.js").ButtonGuiOptions} buttonArgs
 	 */
 	constructor(PopoverConstructor, popoverManager, buttonArgs) {
+		const {onClick} = buttonArgs;
+
+		buttonArgs.onClick = (ctx) => {
+			if(!this.#popoverConstructorInstance || this.#popoverConstructorInstance.destroyed) {
+				this.#popoverConstructorInstance = /** @type {T} */ (popoverManager.addPopover(PopoverConstructor));
+				this.#onPopoverCreatedCbs.forEach(cb => cb(/** @type {T} */ (this.#popoverConstructorInstance)));
+			}
+
+			this.#popoverConstructorInstance?.close();
+			this.#popoverConstructorInstance = null;
+			if (onClick) {
+				onClick(ctx);
+			}
+		};
+
 		super(buttonArgs);
 
 		this.#popoverManager = popoverManager;
@@ -30,26 +51,5 @@ export class PopoverToggleButton extends Button {
 		 * @type {new (...args: any[]) => T} PopoverConstructor
 		 */
 		this.PopoverConstructor = PopoverConstructor;
-	}
-
-	/**
-	 * Toggles the associated popover. Additionally runs callback on popover instantiation, if provided.
-	 *
-	 * @param {((popover: T) => void) | null} successCallback optional callback function to run if the popover is added to the popoverManager.
-	 * @returns {boolean} true if the popover is added to the popoverManager, false if the popover is removed from the popoverManager.
-	 */
-	togglePopover(successCallback = null) {
-		if (!this.#popoverConstructorInstance || this.#popoverConstructorInstance.destroyed) {
-			this.#popoverConstructorInstance = /** @type {T} */ (this.#popoverManager.addPopover(this.PopoverConstructor));
-			if (successCallback) {
-				successCallback(this.#popoverConstructorInstance);
-			}
-			return true;
-		}
-
-		this.#popoverConstructorInstance.close();
-		this.#popoverConstructorInstance = null;
-
-		return false;
 	}
 }
