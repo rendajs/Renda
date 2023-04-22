4894a2cd0410c44a174a198a0a872aa58c0fd069
Jesper van den Ende <jespertheend@gmail.com>
2023-04-23 01:28:06 +0200

    Add textgui test

---
+++ test/unit/studio/src/ui/TextGui.test.js
@@ -0,0 +1,27 @@
+import {assertSpyCall, assertSpyCalls} from "std/testing/mock.ts";
+import {TextGui} from "../../../../../studio/src/ui/TextGui.js";
+import {runWithDom} from "../../shared/runWithDom.js";
+import {createOnChangeEventSpy} from "./shared.js";
+
+Deno.test({
+	name: "Fires events when changed",
+	fn() {
+		runWithDom(() => {
+			const gui = new TextGui();
+			const changeSpy = createOnChangeEventSpy(gui);
+
+			gui.el.value = "hello";
+			gui.el.dispatchEvent(new Event("change"));
+
+			assertSpyCalls(changeSpy, 1);
+			assertSpyCall(changeSpy, 0, {
+				args: [
+					{
+						value: "hello",
+						trigger: "user",
+					},
+				],
+			});
+		});
+	},
+});
