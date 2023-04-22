eaee38f827ba48c6128646f2e9ac3417eda72186
Jesper van den Ende <jespertheend@gmail.com>
2023-04-22 02:56:48 +0200

    add github workflow for releasing to npm

--- scripts/buildNpmPackage.js
+++ scripts/buildNpmPackage.js
@@ -20,7 +20,8 @@ const engineSource = await buildEngine();
 await fs.ensureDir(distPath);
 await Deno.writeTextFile(path.resolve(distPath, "renda.js"), engineSource);
 
-const version = Deno.args[0];
+let version = Deno.args[0];
+if (version && version.startsWith("v")) version = version.slice(1);
 if (!version) {
 	throw new Error("No version provided, specify the package.json version using the first command. I.e. `deno task build-npm-package 1.2.3`");
 }
