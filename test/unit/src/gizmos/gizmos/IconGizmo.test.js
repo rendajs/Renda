1cf64f6af90473cc911df6eb7382d578f913381a
Jesper van den Ende <jespertheend@gmail.com>
2023-04-29 12:17:10 +0200

    add tests

---
+++ test/unit/src/gizmos/gizmos/IconGizmo.test.js
@@ -0,0 +1,36 @@
+import {assertEquals, assertStrictEquals} from "std/testing/asserts.ts";
+import {IconGizmo, VertexState} from "../../../../../src/mod.js";
+
+Deno.test({
+	name: "billboardVertexState from gizmomanager is used",
+	fn() {
+		const mockGizmoManager = /** @type {import("../../../../../src/gizmos/GizmoManager.js").GizmoManager} */ ({
+			billboardVertexState: new VertexState(),
+		});
+		const gizmo = new IconGizmo(mockGizmoManager);
+		assertStrictEquals(gizmo.mesh.vertexState, mockGizmoManager.billboardVertexState);
+	},
+});
+
+Deno.test({
+	name: "Works when billboardVertexState is not available yet",
+	fn() {
+		const mockGizmoManager = /** @type {import("../../../../../src/gizmos/GizmoManager.js").GizmoManager} */ ({
+			billboardVertexState: null,
+		});
+		const gizmo = new IconGizmo(mockGizmoManager);
+		gizmo.addCircle(10, 1);
+		gizmo.updateMesh();
+	},
+});
+
+Deno.test({
+	name: "addCircle adds a circle",
+	fn() {
+		const mockGizmoManager = /** @type {import("../../../../../src/gizmos/GizmoManager.js").GizmoManager} */ ({});
+		const gizmo = new IconGizmo(mockGizmoManager);
+		gizmo.addCircle(10, 1);
+		gizmo.updateMesh();
+		assertEquals(gizmo.mesh.vertexCount, 10);
+	},
+});
