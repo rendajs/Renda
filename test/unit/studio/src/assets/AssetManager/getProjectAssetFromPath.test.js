89104a5bf7eef1f3a330b0a486228ed3e048b031
Jesper van den Ende <jespertheend@gmail.com>
2023-04-29 12:44:21 +0200

    test: Use testTypes() in more places

--- test/unit/studio/src/assets/AssetManager/getProjectAssetFromPath.test.js
+++ test/unit/studio/src/assets/AssetManager/getProjectAssetFromPath.test.js
@@ -3,7 +3,7 @@ import {assertEquals, assertExists, assertRejects} from "std/testing/asserts.ts"
 import {ProjectAssetTypeEntity} from "../../../../../../studio/src/assets/projectAssetType/ProjectAssetTypeEntity.js";
 import {ProjectAssetTypeMaterial} from "../../../../../../studio/src/assets/projectAssetType/ProjectAssetTypeMaterial.js";
 import {injectMockStudioInstance} from "../../../../../../studio/src/studioInstance.js";
-import {assertIsType} from "../../../../shared/typeAssertions.js";
+import {assertIsType, testTypes} from "../../../../shared/typeAssertions.js";
 import {createMockProjectAssetType} from "../../../shared/createMockProjectAssetType.js";
 import {BASIC_ASSET_PATH, BASIC_PROJECTASSETTYPE, NON_EXISTENT_ASSET_PATH, basicSetup} from "./shared.js";
 
@@ -104,68 +104,69 @@ Deno.test({
 	},
 });
 
-// No runtime behaviour is being tested here, only types.
-// eslint-disable-next-line no-unused-vars
-async function testTypes() {
-	const {assetManager} = await basicSetup();
-	const projectAssetUnknown = /** @type {import("../../../../../../studio/src/assets/ProjectAsset.js").ProjectAsset<import("../../../../../../studio/src/assets/projectAssetType/ProjectAssetType.js").ProjectAssetTypeUnknown>} */ ({});
-	const projectAssetUnknownOrNull = /** @type {import("../../../../../../studio/src/assets/ProjectAsset.js").ProjectAsset<import("../../../../../../studio/src/assets/projectAssetType/ProjectAssetType.js").ProjectAssetTypeUnknown>?} */ ({});
-	const projectAssetMaterial = /** @type {import("../../../../../../studio/src/assets/ProjectAsset.js").ProjectAsset<ProjectAssetTypeMaterial>} */ ({});
-	const projectAssetMaterialOrNull = /** @type {typeof projectAssetMaterial | null} */ ({});
-	const projectAssetEntity = /** @type {import("../../../../../../studio/src/assets/ProjectAsset.js").ProjectAsset<ProjectAssetTypeEntity>} */ ({});
-	const projectAssetEntityOrMaterial = /** @type {import("../../../../../../studio/src/assets/ProjectAsset.js").ProjectAsset<ProjectAssetTypeMaterial | ProjectAssetTypeEntity>} */ ({});
+testTypes({
+	name: "getProjectAssetFromPath() has the correct return types",
+	async fn() {
+		const {assetManager} = await basicSetup();
+		const projectAssetUnknown = /** @type {import("../../../../../../studio/src/assets/ProjectAsset.js").ProjectAsset<import("../../../../../../studio/src/assets/projectAssetType/ProjectAssetType.js").ProjectAssetTypeUnknown>} */ ({});
+		const projectAssetUnknownOrNull = /** @type {import("../../../../../../studio/src/assets/ProjectAsset.js").ProjectAsset<import("../../../../../../studio/src/assets/projectAssetType/ProjectAssetType.js").ProjectAssetTypeUnknown>?} */ ({});
+		const projectAssetMaterial = /** @type {import("../../../../../../studio/src/assets/ProjectAsset.js").ProjectAsset<ProjectAssetTypeMaterial>} */ ({});
+		const projectAssetMaterialOrNull = /** @type {typeof projectAssetMaterial | null} */ ({});
+		const projectAssetEntity = /** @type {import("../../../../../../studio/src/assets/ProjectAsset.js").ProjectAsset<ProjectAssetTypeEntity>} */ ({});
+		const projectAssetEntityOrMaterial = /** @type {import("../../../../../../studio/src/assets/ProjectAsset.js").ProjectAsset<ProjectAssetTypeMaterial | ProjectAssetTypeEntity>} */ ({});
 
-	// Default assertions
-	const asset1 = await assetManager.getProjectAssetFromPath(BASIC_ASSET_PATH);
-	assertIsType(projectAssetUnknownOrNull, asset1);
-	assertIsType(asset1, null);
-	assertIsType(asset1, projectAssetUnknown);
-	// @ts-expect-error Verify that the type isn't 'any'
-	assertIsType(true, asset1);
+		// Default assertions
+		const asset1 = await assetManager.getProjectAssetFromPath(BASIC_ASSET_PATH);
+		assertIsType(projectAssetUnknownOrNull, asset1);
+		assertIsType(asset1, null);
+		assertIsType(asset1, projectAssetUnknown);
+		// @ts-expect-error Verify that the type isn't 'any'
+		assertIsType(true, asset1);
 
-	// assertExists true
-	const asset2 = await assetManager.getProjectAssetFromPath(BASIC_ASSET_PATH, {
-		assertionOptions: {
-			assertExists: true,
-		},
-	});
-	assertIsType(projectAssetUnknown, asset2);
-	// @ts-expect-error Verify that the type isn't 'any'
-	assertIsType(true, asset2);
+		// assertExists true
+		const asset2 = await assetManager.getProjectAssetFromPath(BASIC_ASSET_PATH, {
+			assertionOptions: {
+				assertExists: true,
+			},
+		});
+		assertIsType(projectAssetUnknown, asset2);
+		// @ts-expect-error Verify that the type isn't 'any'
+		assertIsType(true, asset2);
 
-	// assertAssetType material
-	const asset3 = await assetManager.getProjectAssetFromPath(BASIC_ASSET_PATH, {
-		assertionOptions: {
-			assertAssetType: [ProjectAssetTypeMaterial],
-			assertExists: true,
-		},
-	});
-	assertIsType(projectAssetMaterial, asset3);
-	// @ts-expect-error Verify that the type isn't 'any'
-	assertIsType(true, asset3);
+		// assertAssetType material
+		const asset3 = await assetManager.getProjectAssetFromPath(BASIC_ASSET_PATH, {
+			assertionOptions: {
+				assertAssetType: [ProjectAssetTypeMaterial],
+				assertExists: true,
+			},
+		});
+		assertIsType(projectAssetMaterial, asset3);
+		// @ts-expect-error Verify that the type isn't 'any'
+		assertIsType(true, asset3);
 
-	// assertAssetType material or null
-	const asset4 = await assetManager.getProjectAssetFromPath(BASIC_ASSET_PATH, {
-		assertionOptions: {
-			assertAssetType: ProjectAssetTypeMaterial,
-		},
-	});
-	assertIsType(projectAssetMaterialOrNull, asset4);
-	assertIsType(asset4, null);
-	assertIsType(asset4, projectAssetMaterial);
-	// @ts-expect-error Verify that the type isn't 'any'
-	assertIsType(true, asset4);
+		// assertAssetType material or null
+		const asset4 = await assetManager.getProjectAssetFromPath(BASIC_ASSET_PATH, {
+			assertionOptions: {
+				assertAssetType: ProjectAssetTypeMaterial,
+			},
+		});
+		assertIsType(projectAssetMaterialOrNull, asset4);
+		assertIsType(asset4, null);
+		assertIsType(asset4, projectAssetMaterial);
+		// @ts-expect-error Verify that the type isn't 'any'
+		assertIsType(true, asset4);
 
-	// assertAssetType material or entity
-	const asset5 = await assetManager.getProjectAssetFromPath(BASIC_ASSET_PATH, {
-		assertionOptions: {
-			assertAssetType: [ProjectAssetTypeMaterial, ProjectAssetTypeEntity],
-			assertExists: true,
-		},
-	});
-	assertIsType(projectAssetEntityOrMaterial, asset5);
-	assertIsType(asset5, projectAssetMaterial);
-	assertIsType(asset5, projectAssetEntity);
-	// @ts-expect-error Verify that the type isn't 'any'
-	assertIsType(true, asset5);
-}
+		// assertAssetType material or entity
+		const asset5 = await assetManager.getProjectAssetFromPath(BASIC_ASSET_PATH, {
+			assertionOptions: {
+				assertAssetType: [ProjectAssetTypeMaterial, ProjectAssetTypeEntity],
+				assertExists: true,
+			},
+		});
+		assertIsType(projectAssetEntityOrMaterial, asset5);
+		assertIsType(asset5, projectAssetMaterial);
+		assertIsType(asset5, projectAssetEntity);
+		// @ts-expect-error Verify that the type isn't 'any'
+		assertIsType(true, asset5);
+	},
+});
