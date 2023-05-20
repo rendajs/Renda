01179b98af013eab0bc2af2a6a7aa9b6748d879d
Jesper van den Ende <jespertheend@gmail.com>
2023-05-20 22:08:58 +0200

    fix some type errors

--- studio/src/network/studioConnections/protocolRequestHandlers/getRequestHandlerType.ts
+++ studio/src/network/studioConnections/protocolRequestHandlers/getRequestHandlerType.ts
@@ -39,12 +39,16 @@ export type getRequestHandlerArgs<T extends HandlerCommands> =
 			FullArgs extends [meta: RequestMetaData, ...rest: infer Args] ?
 				[...args: Args] :
 				never :
-			[...args: FullArgs] :
+			FullArgs extends any [] ?
+				[...args: FullArgs] :
+				[] :
 		never;
 
 export type getRequestHandlerReturnType<T extends HandlerCommands> =
 	getRequestHandlerType<T> extends ProtocolManagerRequestHandler<string, any, any, infer RequestSignature, infer ResponseSignature> ?
 		hasHandleResponse<T> extends true ?
-			ReturnType<ResponseSignature> :
+			ResponseSignature extends (...args: any) => any ?
+				ReturnType<ResponseSignature> :
+				never :
 			ReturnType<RequestSignature> :
 		never;
