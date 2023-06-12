57a641996241d6bdc969ca7b77bde63fe3c0cb24
Jesper van den Ende <jespertheend@gmail.com>
2023-06-12 23:26:54 +0200

    feat: Show an error message when initializing the renderer fails

--- studio/src/styles/shadowStyles.js
+++ studio/src/styles/shadowStyles.js
@@ -1,3 +1,13 @@
-// @ts-ignore
+/**
+ * @fileoverview
+ * These style sheets are not appended to the global `document.adoptedStyleSheets` array, but instead are
+ * imported across different files to be used in a shadow tree.
+ */
+
+// @ts-nocheck
+
 import contentWindowHistorySheet from "../windowManagement/contentWindows/ContentWindowHistory.css" assert {type: "css"};
 export {contentWindowHistorySheet};
+
+import contentWindowEntityEditorSheet from "../windowManagement/contentWindows/ContentWindowEntityEditor/ContentWindowEntityEditor.css" assert {type: "css"};
+export {contentWindowEntityEditorSheet};
