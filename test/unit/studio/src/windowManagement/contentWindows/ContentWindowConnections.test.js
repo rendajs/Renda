6ebdb37d424311122689b3fcd51ca79946972012
Jesper van den Ende <jespertheend@gmail.com>
2023-11-01 23:41:27 +0100

    Add ContentWindowConnections tests

--- test/unit/studio/src/windowManagement/contentWindows/ContentWindowConnections.test.js
+++ test/unit/studio/src/windowManagement/contentWindows/ContentWindowConnections.test.js
@@ -8,12 +8,17 @@ import {PropertiesTreeViewEntry} from "../../../../../../studio/src/ui/propertie
 import {Button} from "../../../../../../studio/src/ui/Button.js";
 import {PreferencesManager} from "../../../../../../studio/src/preferences/PreferencesManager.js";
 import {getMockWindowManager} from "../shared.js";
+import {TextGui} from "../../../../../../studio/src/ui/TextGui.js";
+import {assertSpyCall, assertSpyCalls, spy} from "std/testing/mock.ts";
 
 /**
  * @typedef ContentWindowConnectionsTestContext
  * @property {ContentWindowConnections} contentWindow
+ * @property {import("../../../../../../studio/src/network/studioConnections/StudioConnectionsManager.js").StudioConnectionsManager} studioConnectionsManager
+ * @property {LabelGui} webRtcStatusLabel
  * @property {import("../../../../../../studio/src/ui/TreeView.js").TreeView} studiosListTreeView
  * @property {() => void} fireOnAvailableConnectionsChanged
+ * @property {(status: import("../../../../../../src/network/studioConnections/discoveryMethods/WebRtcDiscoveryMethod.js").DiscoveryServerStatusType) => void} fireOnWebRtcDiscoveryserverStatusChange
  */
 
 /**
@@ -30,18 +35,37 @@ async function basicTest({
 	installFakeDocument();
 
 	try {
+		/** @type {import("../../../../../../src/network/studioConnections/discoveryMethods/WebRtcDiscoveryMethod.js").DiscoveryServerStatusType} */
+		let webRtcDiscoveryServerStatus = "disconnected";
 		/** @type {Set<() => void>} */
 		const onAvailableConnectionsChangedCbs = new Set();
+		/** @type {Set<import("../../../../../../src/network/studioConnections/discoveryMethods/WebRtcDiscoveryMethod.js").OnDiscoveryManagerWebRtcStatusChangeCallback>} */
+		const onWebRtcDiscoveryServerStatusChangeCbs = new Set();
 		const mockStudioInstance = /** @type {import("../../../../../../studio/src/Studio.js").Studio} */ ({
 			studioConnectionsManager: {
 				getDefaultWebRtcDiscoveryEndpoint() {
 					return "discovery.renda.studio";
 				},
-				onWebRtcDiscoveryServerStatusChange(cb) {},
-				onConnectionsChanged(cb) {},
+				onWebRtcDiscoveryServerStatusChange(cb) {
+					onWebRtcDiscoveryServerStatusChangeCbs.add(cb);
+				},
+				removeOnWebRtcDiscoveryServerStatusChange(cb) {
+					onWebRtcDiscoveryServerStatusChangeCbs.delete(cb);
+				},
+				onConnectionsChanged(cb) {
+					onAvailableConnectionsChangedCbs.add(cb);
+				},
+				removeOnConnectionsChanged(cb) {
+					onAvailableConnectionsChangedCbs.delete(cb);
+				},
 				*getConnections() {
 					yield* connections;
 				},
+				get webRtcDiscoveryServerStatus() {
+					return webRtcDiscoveryServerStatus;
+				},
+				setWebRtcDiscoveryEndpoint(endpoint) {},
+				requestConnection(otherClientUuid) {},
 			},
 			projectManager: {
 				currentProjectIsRemote,
@@ -60,13 +84,20 @@ async function basicTest({
 			checkAllChildren: false,
 		});
 		const studiosListTreeView = getChildTreeViewFromIndices(contentWindow.studioClientConnectionTreeView, 0);
+		const webRtcStatusLabel = contentWindow.discoveryServerStatusLabel.gui;
 
 		/** @type {ContentWindowConnectionsTestContext} */
 		const testContext = {
 			contentWindow,
 			studiosListTreeView,
+			webRtcStatusLabel,
+			studioConnectionsManager: mockStudioInstance.studioConnectionsManager,
 			fireOnAvailableConnectionsChanged() {
 				onAvailableConnectionsChangedCbs.forEach(cb => cb());
+			},
+			fireOnWebRtcDiscoveryserverStatusChange(status) {
+				webRtcDiscoveryServerStatus = status;
+				onWebRtcDiscoveryServerStatusChangeCbs.forEach(cb => cb(status));
 			},
 		};
 
@@ -113,7 +144,10 @@ function assertConnectionTreeView(treeView, projectName, connectionType, status,
 	const connectButtonTreeView = getChildTreeViewFromIndices(treeView, 2);
 	assertInstanceOf(connectButtonTreeView, PropertiesTreeViewEntry);
 	assertInstanceOf(connectButtonTreeView.gui, Button);
+	const connectButtonGui = connectButtonTreeView.gui;
 	assertEquals(connectButtonTreeView.gui.disabled, !connectButtonEnabled);
+
+	return {connectButtonGui};
 }
 
 Deno.test({
@@ -214,8 +248,144 @@ Deno.test({
 });
 
 Deno.test({
+	name: "Unknown connection type",
+	async fn() {
+		await basicTest({
+			connections: [
+				{
+					id: "uuid1",
+					clientType: "studio-host",
+					connectionState: "disconnected",
+					connectionType: "renda:unknown",
+					projectMetadata: {
+						fileSystemHasWritePermissions: true,
+						name: "project name",
+						uuid: "uuid",
+					},
+				},
+			],
+			fn({studiosListTreeView}) {
+				const studioTreeView = getChildTreeViewFromIndices(studiosListTreeView, 0);
+				assertConnectionTreeView(studioTreeView, "project name", "renda:unknown", "Available", "", true);
+			},
+		});
+	},
+});
+
+Deno.test({
+	name: "Studio-client with no open project",
+	async fn() {
+		await basicTest({
+			connections: [
+				{
+					id: "uuid1",
+					clientType: "studio-client",
+					connectionState: "disconnected",
+					connectionType: "renda:webrtc",
+					projectMetadata: null,
+				},
+			],
+			fn({studiosListTreeView}) {
+				const studioTreeView = getChildTreeViewFromIndices(studiosListTreeView, 0);
+				assertConnectionTreeView(studioTreeView, "Studio Client", "WebRTC", "Unavailable", "This connection is a studio instance without an open project. Connections can only be initiated from the other end.", false);
+			},
+		});
+	},
+});
+
+Deno.test({
+	name: "Studio-client with an open project",
+	async fn() {
+		await basicTest({
+			connections: [
+				{
+					id: "uuid1",
+					clientType: "studio-client",
+					connectionState: "disconnected",
+					connectionType: "renda:webrtc",
+					projectMetadata: {
+						fileSystemHasWritePermissions: true,
+						name: "my project",
+						uuid: "uuid",
+					},
+				},
+			],
+			fn({studiosListTreeView}) {
+				const studioTreeView = getChildTreeViewFromIndices(studiosListTreeView, 0);
+				assertConnectionTreeView(studioTreeView, "Studio Client", "WebRTC", "Unavailable", "This connection is a studio instance without an open project. Connections can only be initiated from the other end.", false);
+			},
+		});
+	},
+});
+
+Deno.test({
+	name: "Connecting to a connection by clicking the button",
+	async fn() {
+		await basicTest({
+			connections: [
+				{
+					id: "connection uuid",
+					clientType: "studio-host",
+					connectionState: "disconnected",
+					connectionType: "renda:webrtc",
+					projectMetadata: {
+						fileSystemHasWritePermissions: true,
+						name: "my project",
+						uuid: "project uuid",
+					},
+				},
+			],
+			fn({studiosListTreeView, studioConnectionsManager}) {
+				const studioTreeView = getChildTreeViewFromIndices(studiosListTreeView, 0);
+				const {connectButtonGui} = assertConnectionTreeView(studioTreeView, "my project", "WebRTC", "Available", "", true);
+				const requestConnectionSpy = spy(studioConnectionsManager, "requestConnection");
+
+				connectButtonGui.click();
+
+				assertSpyCalls(requestConnectionSpy, 1);
+				assertSpyCall(requestConnectionSpy, 0, {
+					args: ["connection uuid"],
+				});
+			},
+		});
+	},
+});
+
+Deno.test({
+	name: "Changing connection status on an active connection",
+	async fn() {
+		/** @type {import("../../../../../../studio/src/network/studioConnections/StudioConnectionsManager.js").StudioConnectionData} */
+		const connection = {
+			id: "uuid1",
+			clientType: "studio-host",
+			connectionState: "disconnected",
+			connectionType: "renda:webrtc",
+			projectMetadata: {
+				fileSystemHasWritePermissions: true,
+				name: "my project",
+				uuid: "project uuid",
+			},
+		};
+		await basicTest({
+			connections: [connection],
+			fn({studiosListTreeView, fireOnAvailableConnectionsChanged}) {
+				const studioTreeView = getChildTreeViewFromIndices(studiosListTreeView, 0);
+				assertConnectionTreeView(studioTreeView, "my project", "WebRTC", "Available", "", true);
+
+				connection.connectionState = "connecting";
+				fireOnAvailableConnectionsChanged();
+				assertConnectionTreeView(studioTreeView, "my project", "WebRTC", "Connecting", "", false);
+
+				connection.connectionState = "connected";
+				fireOnAvailableConnectionsChanged();
+				assertConnectionTreeView(studioTreeView, "my project", "WebRTC", "Connected", "", false);
+			},
+		});
+	},
+});
+
+Deno.test({
 	name: "Old connections get removed",
-	ignore: true,
 	async fn() {
 		/** @type {import("../../../../../../studio/src/network/studioConnections/StudioConnectionsManager.js").StudioConnectionData[]} */
 		const connections = [
@@ -245,14 +415,26 @@ Deno.test({
 });
 
 Deno.test({
+	name: "WebRTC discovery status changes",
+	async fn() {
+		await basicTest({
+			fn({webRtcStatusLabel, fireOnWebRtcDiscoveryserverStatusChange}) {
+				assertEquals(webRtcStatusLabel.value, "disconnected");
+				fireOnWebRtcDiscoveryserverStatusChange("connecting");
+				assertEquals(webRtcStatusLabel.value, "connecting");
+			},
+		});
+	},
+});
+
+Deno.test({
 	name: "Callbacks are unregistered when destructed",
-	ignore: true,
 	async fn() {
 		/** @type {import("../../../../../../studio/src/network/studioConnections/StudioConnectionsManager.js").StudioConnectionData[]} */
 		const connections = [];
 		await basicTest({
 			connections,
-			fn({contentWindow, studiosListTreeView, fireOnAvailableConnectionsChanged}) {
+			fn({contentWindow, studiosListTreeView, webRtcStatusLabel, fireOnAvailableConnectionsChanged, fireOnWebRtcDiscoveryserverStatusChange}) {
 				connections.push({
 					id: "uuid1",
 					clientType: "studio-host",
@@ -263,11 +445,38 @@ Deno.test({
 				fireOnAvailableConnectionsChanged();
 				assertEquals(studiosListTreeView.children.length, 1);
 
+				assertEquals(webRtcStatusLabel.value, "disconnected");
+				fireOnWebRtcDiscoveryserverStatusChange("connecting");
+				assertEquals(webRtcStatusLabel.value, "connecting");
+
 				contentWindow.destructor();
 
 				connections.pop();
 				fireOnAvailableConnectionsChanged();
 				assertEquals(studiosListTreeView.children.length, 1);
+
+				fireOnWebRtcDiscoveryserverStatusChange("connected");
+				assertEquals(webRtcStatusLabel.value, "connecting");
+			},
+		});
+	},
+});
+
+Deno.test({
+	name: "Discovery endpoint changes are passed to the studioConnectionsManager",
+	async fn() {
+		await basicTest({
+			fn({contentWindow, studioConnectionsManager}) {
+				const endPointTreeView = getChildTreeViewFromIndices(contentWindow.headerTreeView, 0);
+				const setEndpointSpy = spy(studioConnectionsManager, "setWebRtcDiscoveryEndpoint");
+				assertInstanceOf(endPointTreeView, PropertiesTreeViewEntry);
+				assertInstanceOf(endPointTreeView.gui, TextGui);
+
+				endPointTreeView.gui.setValue("new endpoint");
+				assertSpyCalls(setEndpointSpy, 1);
+				assertSpyCall(setEndpointSpy, 0, {
+					args: ["new endpoint"],
+				});
 			},
 		});
 	},
