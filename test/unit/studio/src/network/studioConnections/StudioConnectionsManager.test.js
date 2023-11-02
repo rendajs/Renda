df58a3ca7bdbfe29662f385be876802113de2da0
Jesper van den Ende <jespertheend@gmail.com>
2023-11-02 21:17:47 +0100

    Initial setup for StudioConnectionsManager tests

---
+++ test/unit/studio/src/network/studioConnections/StudioConnectionsManager.test.js
@@ -0,0 +1,72 @@
+import {Importer} from "fake-imports";
+import {assertSpyCall, assertSpyCalls, spy, stub} from "std/testing/mock.ts";
+import {assertEquals} from "std/testing/asserts.ts";
+import { createPreferencesManager } from "../../../shared/createPreferencesManager.js";
+
+const importer = new Importer(import.meta.url);
+importer.makeReal("../../../../../../src/mod.js");
+importer.makeReal("../../../../../../studio/src/studioInstance.js");
+importer.redirectModule("../../../../../../src/inspector/InternalDiscoveryManager.js", "./MockInternalDiscoveryManager.js");
+
+/** @type {import("../../../../../../studio/src/network/studioConnections/StudioConnectionsManager.js")} */
+const StudioConnectionsManagerMod = await importer.import("../../../../../../studio/src/network/studioConnections/StudioConnectionsManager.js");
+const {StudioConnectionsManager} = StudioConnectionsManagerMod;
+
+/**
+ * @typedef StudioConnectionsManagerTestContext
+ * @property {import("../../../../../../studio/src/network/studioConnections/StudioConnectionsManager.js").StudioConnectionsManager} manager
+ */
+
+const OTHER_CLIENT_ID = "other_client_id";
+
+/**
+ * @param {object} options
+ * @param {(ctx: StudioConnectionsManagerTestContext) => void} options.fn
+ */
+function basicTest({
+	fn,
+}) {
+	let randomUuid = 0;
+	const mockUuid = stub(crypto, "randomUUID", () => {
+		randomUuid++;
+		return "random_uuid_" + randomUuid;
+	});
+	const oldLocation = window.location;
+	try {
+		window.location = /** @type {Location} */ ({
+			href: "https://renda.studio/",
+		});
+
+		const mockProjectManager = /** @type {import("../../../../../../studio/src/projectSelector/ProjectManager.js").ProjectManager} */ ({
+			onProjectOpen(cb) {},
+			onRootHasWritePermissionsChange(cb) {},
+			onProjectOpenEntryChange(cb) {},
+		})
+		const {preferencesManager} = createPreferencesManager({
+			"studioConnections.allowInternalIncoming": {
+				type: "boolean",
+			},
+			"studioConnections.allowRemoteIncoming": {
+				type: "boolean",
+			}
+		});
+
+		const manager = new StudioConnectionsManager(mockProjectManager, preferencesManager);
+
+		fn({manager});
+	} finally {
+		window.location = oldLocation;
+		mockUuid.restore();
+	}
+}
+
+Deno.test({
+	name: "creates a discovery manager",
+	fn() {
+		basicTest({
+			fn({manager}) {
+
+			}
+		})
+	}
+})
