a54754055f0e9201ac59adc894b05bd84f2e38ae
Jesper van den Ende <jespertheend@gmail.com>
2023-12-20 14:55:12 +0100

    Add some missing exports

---
+++ src/rendering/renderers/webGpu/mod.js
@@ -0,0 +1,4 @@
+export * from "./WebGpuRenderer.js";
+export {WebGpuMaterialMapType} from "./WebGpuMaterialMapType.js";
+export {WebGpuMaterialMapTypeLoader} from "./WebGpuMaterialMapTypeLoader.js";
+export {WebGpuPipelineConfig} from "./WebGpuPipelineConfig.js";
