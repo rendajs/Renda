0143a42e5b4d9252791bb1bcb26b5ace81ec5251
Jesper van den Ende <jespertheend@gmail.com>
2024-03-11 02:54:42 +0100

    lint

--- scripts/shared/verifyVersion.js
+++ scripts/shared/verifyVersion.js
@@ -2,6 +2,6 @@ import { BUILD_VERSION_STRING } from "../../studio/src/studioDefines.js";
 
 const expectedVersion = Deno.args[0];
 if (BUILD_VERSION_STRING != expectedVersion) {
-	console.error(`Expected version to match "${expectedVersion}". Please update studioDefines.js`)
+	console.error(`Expected version to match "${expectedVersion}". Please update studioDefines.js`);
 	Deno.exit(1);
 }
