7c7b9acfe3cf65f36257934e723235474902eb4d
Jesper van den Ende <jespertheend@users.noreply.github.com>
2024-03-11 02:57:16 +0100

    chore: Addgithub workflow for preparing a release (#895)

---
+++ scripts/shared/verifyVersion.js
@@ -0,0 +1,7 @@
+import { BUILD_VERSION_STRING } from "../../studio/src/studioDefines.js";
+
+const expectedVersion = Deno.args[0];
+if (BUILD_VERSION_STRING != expectedVersion) {
+	console.error(`Expected version to match "${expectedVersion}". Please update studioDefines.js`);
+	Deno.exit(1);
+}
