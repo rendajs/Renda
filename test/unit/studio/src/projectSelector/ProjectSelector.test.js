eb3232847158d8c8d47625fd831da3c77f71b8aa
Jesper van den Ende <jespertheend@gmail.com>
2024-03-23 19:04:07 +0100

    Add Linux test

--- test/unit/studio/src/projectSelector/ProjectSelector.test.js
+++ test/unit/studio/src/projectSelector/ProjectSelector.test.js
@@ -143,6 +143,13 @@ Deno.test({
 				maxTouchPoints: 1,
 				expected: "Get the Windows App",
 			},
+			{
+				// Linux
+				userAgent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+				platform: "Linux x86_64",
+				maxTouchPoints: 0,
+				expected: "Get the Linux App",
+			},
 		];
 
 		for (const test of tests) {
