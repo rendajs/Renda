89a6788b7e795ccb03501a4ed1d0dc0a38d3cd1e
Jesper van den Ende <jespertheend@users.noreply.github.com>
2024-11-14 16:52:16 +0100

    fix(math): Fix slerpQuaternions results being wrong at certain angles (#961)

--- test/unit/src/math/Quat.test.js
+++ test/unit/src/math/Quat.test.js
@@ -33,9 +33,9 @@ Deno.test({
  * @param {number} t
  * @param {Quat} expected
  */
-function basicSlerpTest(a, b, t, expected) {
+function basicSlerpTest(a, b, t, expected, tolerance = 0.00001) {
 	const result = Quat.slerpQuaternions(a, b, t);
-	assertQuatAlmostEquals(result, expected);
+	assertQuatAlmostEquals(result, expected, tolerance);
 }
 
 Deno.test({
@@ -65,6 +65,21 @@ Deno.test({
 });
 
 Deno.test({
+	name: "two quaternions that are very close to each other",
+	fn() {
+		const a = new Quat();
+		const b = Quat.fromAxisAngle(0, 1, 0, 0.00000003);
+		basicSlerpTest(a, b, 0, a, 0);
+		basicSlerpTest(a, b, 0.1, new Quat(0, 1.5e-9, 0, 1), 0);
+		basicSlerpTest(a, b, 0.25, new Quat(0, 3.75e-9, 0, 1), 0);
+		basicSlerpTest(a, b, 0.5, new Quat(0, 7.5e-9, 0, 1), 0);
+		basicSlerpTest(a, b, 0.75, new Quat(0, 1.1249999999999998e-8, 0, 0.9999999999999999), 0);
+		basicSlerpTest(a, b, 0.9, new Quat(0, 1.3499999999999998e-8, 0, 0.9999999999999999), 0);
+		basicSlerpTest(a, b, 1, b, 0);
+	},
+});
+
+Deno.test({
 	name: "slerp that results in a negative cosHalfTheta",
 	fn() {
 		const a = Quat.fromAxisAngle(0, 1, 0, 2);
@@ -75,11 +90,29 @@ Deno.test({
 });
 
 Deno.test({
+	name: "another slerp that results in a negative cosHalfTheta",
+	fn() {
+		const a = new Quat();
+		const b = Quat.fromAxisAngle(1, 1, 1, 4);
+		basicSlerpTest(a, b, 0.219, Quat.fromAxisAngle(1, 1, 1, -0.5));
+		basicSlerpTest(a, b, 0.5, Quat.fromAxisAngle(1, 1, 1, -1.14157));
+	},
+});
+
+Deno.test({
 	name: "slerp two quaternions that are the same",
 	fn() {
 		const a = new Quat(0, 0.2, 20, 1);
-		basicSlerpTest(a, a, 0.5, a);
 		const b = new Quat(12, 34, 56, 78);
+		basicSlerpTest(a, a, 0.01, a);
+		basicSlerpTest(b, b, 0.01, b);
+		basicSlerpTest(a, a, 0.1, a);
+		basicSlerpTest(b, b, 0.1, b);
+		basicSlerpTest(a, a, 0.5, a);
 		basicSlerpTest(b, b, 0.5, b);
+		basicSlerpTest(a, a, 0.9, a);
+		basicSlerpTest(b, b, 0.9, b);
+		basicSlerpTest(a, a, 0.99, a);
+		basicSlerpTest(b, b, 0.99, b);
 	},
 });
